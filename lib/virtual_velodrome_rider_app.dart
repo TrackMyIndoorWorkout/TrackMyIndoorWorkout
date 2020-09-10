6101b739e891e05424d8abc767844b0108b45ec6
MrCsabaToth <csaba.toth.us@gmail.com>
2020-09-09 19:28:46 -0700

    MaterialApp -> GetMaterialApp so Get functions like navigation would work

--- lib/virtual_velodrome_rider_app.dart
+++ lib/virtual_velodrome_rider_app.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
-import 'screens/bluetooth_off.dart';
-import 'screens/find_devices.dart';
+import 'package:get/get.dart';
+import 'ui/bluetooth_off.dart';
+import 'ui/find_devices.dart';
 
 class VirtualVelodromeRiderApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       color: Colors.lightBlue,
       home: StreamBuilder<BluetoothState>(
           stream: FlutterBlue.instance.state,
