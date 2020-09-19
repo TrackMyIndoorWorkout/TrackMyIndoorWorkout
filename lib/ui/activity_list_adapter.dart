f70d66d1b3a78f8fb2fb27d304481ff6dc268b7d
MrCsabaToth <csaba.toth.us@gmail.com>
2020-09-18 23:50:13 -0700

    Making the Custom List adapter typed

--- lib/ui/activity_list_adapter.dart
+++ lib/ui/activity_list_adapter.dart
@@ -1,19 +1,20 @@
 import 'package:listview_utils/listview_utils.dart';
 import '../persistence/database.dart';
 
-class ActivityListAdapter implements BaseListAdapter {
+class ActivityListAdapter<Activity> implements BaseListAdapter<Activity> {
   const ActivityListAdapter(this.database);
 
   final AppDatabase database;
 
   @override
-  Future<ListItems> getItems(int offset, int limit) async {
+  Future<ListItems<Activity>> getItems(int offset, int limit) async {
     final data = await database.activityDao.findActivities(offset, limit);
-    return ListItems(data, reachedToEnd: data.length == 0);
+    return ListItems(data as Iterable<Activity>,
+        reachedToEnd: data.length == 0);
   }
 
   @override
-  bool shouldUpdate(ActivityListAdapter old) {
+  bool shouldUpdate(ActivityListAdapter<Activity> old) {
     return false;
   }
 }
