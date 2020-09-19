4934f64a9bc1eee2c6acf8e074964aa000ceb557
MrCsabaToth <csaba.toth.us@gmail.com>
2020-09-19 11:48:42 -0700

    Restrict orientation to portrait (up or down)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'virtual_velodrome_rider_app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(VirtualVelodromeRiderApp());
 }
