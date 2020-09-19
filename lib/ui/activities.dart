1360fcb6211831793071fffffb20c1ad9cf05839
MrCsabaToth <csaba.toth.us@gmail.com>
2020-09-18 23:52:59 -0700

    Completing the Activity tiles and finally wiring up the v1.0 of the Strava TCX uploading code

--- lib/ui/activities.dart
+++ lib/ui/activities.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import 'package:listview_utils/listview_utils.dart';
+import '../persistence/models/activity.dart';
 import '../persistence/database.dart';
+import '../strava/error_codes.dart';
+import '../strava/strava_service.dart';
 import 'activity_list_adapter.dart';
 
 class ActivitiesScreen extends StatefulWidget {
@@ -38,7 +43,7 @@ class ActivitiesScreenState extends State<ActivitiesScreen> {
       backgroundColor: Colors.lightBlue,
       appBar: AppBar(title: Text('Activities')),
       body: SafeArea(
-        child: CustomListView(
+        child: CustomListView<Activity>(
           paginationMode: PaginationMode.page,
           initialOffset: 0,
           loadingBuilder: CustomListLoading.defaultBuilder,
@@ -61,9 +66,46 @@ class ActivitiesScreenState extends State<ActivitiesScreen> {
             child: Text('No activities found'),
           ),
           itemBuilder: (context, _, item) {
+            final activity = item as Activity;
+            final startStamp =
+                DateTime.fromMillisecondsSinceEpoch(activity.start);
+            final dateString = DateFormat.yMd().format(startStamp);
+            final timeString = DateFormat.Hms().format(startStamp);
             return ListTile(
-              title: Text(item['title']),
-              leading: Icon(Icons.assignment),
+              leading: activity.uploaded
+                  ? Icon(Icons.check, color: Colors.lightGreenAccent)
+                  : Icon(Icons.close, color: Colors.red),
+              title: Text('$dateString $timeString on ${activity.deviceName}'),
+              subtitle: Text(
+                  '${activity.elapsed} s, ${activity.distance} m, ${activity.calories} kCal'),
+              trailing: IconButton(
+                icon: Icon(Icons.file_upload,
+                    color: activity.uploaded ? Colors.grey : Colors.green),
+                onPressed: () async {
+                  StravaService stravaService;
+                  if (!Get.isRegistered<StravaService>()) {
+                    stravaService = Get.put<StravaService>(StravaService());
+                  } else {
+                    stravaService = Get.find<StravaService>();
+                  }
+                  final success = await stravaService.login();
+                  if (!success) {
+                    Get.snackbar("Warning", "Strava login unsuccessful");
+                  } else {
+                    final records = await _database.recordDao
+                        .findAllActivityRecords(activity.id);
+                    final statusCode =
+                        await stravaService.upload(activity, records);
+                    if (statusCode == statusOk) {
+                      setState(() {
+                        activity.uploaded = true;
+                      });
+                      activity.uploaded = true;
+                      await _database.activityDao.updateActivity(activity);
+                    }
+                  }
+                },
+              ),
             );
           },
         ),
