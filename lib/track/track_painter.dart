4ccd05b659c77f7a972fa976e1b78b084679fef7
MrCsabaToth <csaba.toth.us@gmail.com>
2020-09-22 23:52:11 -0700

    Speeding up track painting by caching computed objects so they won't have to be recalculated all the time when the size is the same

--- lib/track/track_painter.dart
+++ lib/track/track_painter.dart
@@ -2,48 +2,54 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'constants.dart';
-import '../ui/device.dart';
 
 class TrackPainter extends CustomPainter {
+  static Size _size;
+  static Paint _trackStroke;
+  static Path _trackPath;
+
   @override
   paint(Canvas canvas, Size size) {
-    DeviceState.size = size;
-    final rX = (size.width - 2 * THICK) / (2 * RADIUS_BOOST);
-    final rY =
-        (size.height - 2 * THICK) / (2 * RADIUS_BOOST + pi * LANE_SHRINK);
-    final r = min(rY, rX) * RADIUS_BOOST;
-    DeviceState.radius = r;
-    final offset = Offset(
-        rX > rY ? (size.width - 2 * (THICK + r)) / 2 : 0,
-        rX < rY
-            ? (size.height - 2 * THICK - r * 2 - pi * rX * LANE_SHRINK) / 2
-            : 0);
+    if (_size == null ||
+        size.width != _size.width ||
+        size.height != _size.height) {
+      _size = size;
+      final rX = (size.width - 2 * THICK) / (2 * RADIUS_BOOST);
+      final rY =
+          (size.height - 2 * THICK) / (2 * RADIUS_BOOST + pi * LANE_SHRINK);
+      final r = min(rY, rX) * RADIUS_BOOST;
+      final offset = Offset(
+          rX > rY ? (size.width - 2 * (THICK + r)) / 2 : 0,
+          rX < rY
+              ? (size.height - 2 * THICK - r * 2 - pi * rX * LANE_SHRINK) / 2
+              : 0);
 
-    final trackStroke = Paint()
-      ..color = Color(0x88777777)
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 2 * THICK
-      ..isAntiAlias = true;
+      _trackStroke = Paint()
+        ..color = Color(0x88777777)
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = 2 * THICK
+        ..isAntiAlias = true;
 
-    final topHalfCircleRect = Rect.fromCircle(
-        center: Offset(r + THICK + offset.dx, r + THICK + offset.dy),
-        radius: r);
+      final topHalfCircleRect = Rect.fromCircle(
+          center: Offset(r + THICK + offset.dx, r + THICK + offset.dy),
+          radius: r);
 
-    final bottomHalfCircleRect = Rect.fromCircle(
-        center:
-            Offset(r + THICK + offset.dx, size.height - r - THICK - offset.dy),
-        radius: r);
+      final bottomHalfCircleRect = Rect.fromCircle(
+          center: Offset(
+              r + THICK + offset.dx, size.height - r - THICK - offset.dy),
+          radius: r);
 
-    final trackPath = Path()
-      ..moveTo(THICK + offset.dx,
-          THICK + offset.dy + r * (1 + pi * LANE_SHRINK / RADIUS_BOOST))
-      ..lineTo(THICK + offset.dx, r + THICK + offset.dy)
-      ..arcTo(topHalfCircleRect, pi, pi, true)
-      ..lineTo(2 * r + THICK + offset.dx,
-          THICK + offset.dy + r * (1 + pi * LANE_SHRINK / RADIUS_BOOST))
-      ..arcTo(bottomHalfCircleRect, 0, pi, true);
+      _trackPath = Path()
+        ..moveTo(THICK + offset.dx,
+            THICK + offset.dy + r * (1 + pi * LANE_SHRINK / RADIUS_BOOST))
+        ..lineTo(THICK + offset.dx, r + THICK + offset.dy)
+        ..arcTo(topHalfCircleRect, pi, pi, true)
+        ..lineTo(2 * r + THICK + offset.dx,
+            THICK + offset.dy + r * (1 + pi * LANE_SHRINK / RADIUS_BOOST))
+        ..arcTo(bottomHalfCircleRect, 0, pi, true);
+    }
 
-    canvas.drawPath(trackPath, trackStroke);
+    canvas.drawPath(_trackPath, _trackStroke);
   }
 
   @override
