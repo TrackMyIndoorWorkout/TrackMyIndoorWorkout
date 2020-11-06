b6c839af6136e83cd632192b68a0acf9bb178498
MrCsabaToth <csaba.toth.us@gmail.com>
2020-11-06 11:13:14 -0800

    Correct migration script for introducing and initializing four_cc field

--- lib/persistence/database.dart
+++ lib/persistence/database.dart
@@ -16,6 +16,7 @@ abstract class AppDatabase extends FloorDatabase {
 }
 
 final migration1to2 = Migration(1, 2, (database) async {
-  await database.execute("ALTER TABLE activity ADD COLUMN four_cc TEXT; " +
-      "UPDATE activity SET four_cc='PSCP'");
+  await database.execute(
+      "ALTER TABLE $ACTIVITIES_TABLE_NAME ADD COLUMN four_cc TEXT; " +
+          "UPDATE $ACTIVITIES_TABLE_NAME SET four_cc='PSCP'");
 });
