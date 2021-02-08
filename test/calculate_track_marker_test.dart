3f0a2819149b62dbbc3fa70a06b377aed1a2a85c
MrCsabaToth <csaba.toth.us@gmail.com>
2021-02-08 03:26:28 -0800

    calculateTrackMarker tests for the chicanes

--- test/calculate_track_marker_test.dart
+++ test/calculate_track_marker_test.dart
@@ -21,7 +21,6 @@ void main() {
         lengthFactor: lengthFactor,
       );
       final track = trackMap["Painting"];
-
       final size = Size(minPixel + rnd.nextDouble() * maxPixel,
           minPixel + rnd.nextDouble() * maxPixel);
       RecordingState.trackSize = size;
@@ -77,7 +76,9 @@ void main() {
     });
   });
 
-  test('calculateTrackMarker on first lane is placed proportionally', () async {
+  test(
+      'calculateTrackMarker on the first (top) straight is placed proportionally',
+      () async {
     final rnd = Random();
     final count = rnd.nextInt(99) + 1;
     getRandomDoubles(count, 2, rnd).forEach((lengthFactor) {
@@ -86,7 +87,6 @@ void main() {
         lengthFactor: lengthFactor,
       );
       final track = trackMap["Painting"];
-
       final size = Size(minPixel + rnd.nextDouble() * maxPixel,
           minPixel + rnd.nextDouble() * maxPixel);
       RecordingState.trackSize = size;
@@ -102,7 +102,6 @@ void main() {
           rX > rY ? 0 : (size.height - 2 * (THICK + r)) / 2);
       RecordingState.trackOffset = offset;
       final laps = rnd.nextInt(100);
-
       final positionRatio = rnd.nextDouble();
       final distance =
           laps * TRACK_LENGTH * lengthFactor + positionRatio * track.laneLength;
@@ -116,6 +115,136 @@ void main() {
       expect(marker.dx,
           closeTo(size.width - THICK - offset.dx - r - displacement, 1e-6));
       expect(marker.dy, closeTo(THICK + offset.dy, 1e-6));
+    });
+  });
+
+  test(
+      'calculateTrackMarker on the first (left) chicane is placed proportionally',
+      () async {
+    final rnd = Random();
+    final count = rnd.nextInt(99) + 1;
+    getRandomDoubles(count, 2, rnd).forEach((lengthFactor) {
+      trackMap["Painting"] = TrackDescriptor(
+        radiusBoost: 1 + rnd.nextDouble() / 3,
+        lengthFactor: lengthFactor,
+      );
+      final track = trackMap["Painting"];
+      final size = Size(minPixel + rnd.nextDouble() * maxPixel,
+          minPixel + rnd.nextDouble() * maxPixel);
+      RecordingState.trackSize = size;
+      final rX = (size.width - 2 * THICK) /
+          (2 * track.radiusBoost + pi * track.laneShrink);
+      final rY = (size.height - 2 * THICK) / (2 * track.radiusBoost);
+      final r = min(rY, rX) * track.radiusBoost;
+      RecordingState.trackRadius = r;
+      final offset = Offset(
+          rX < rY
+              ? 0
+              : (size.width - 2 * (THICK + r) - pi * r * track.laneShrink) / 2,
+          rX > rY ? 0 : (size.height - 2 * (THICK + r)) / 2);
+      RecordingState.trackOffset = offset;
+      final laps = rnd.nextInt(100);
+      final positionRatio = rnd.nextDouble();
+      final distance = laps * TRACK_LENGTH * lengthFactor +
+          track.laneLength +
+          positionRatio * track.halfCircle;
+      final trackLength = TRACK_LENGTH * lengthFactor;
+      final d = distance % trackLength;
+      final rad = (1 - (d - track.laneLength) / track.halfCircle) * pi;
+
+      final marker = calculateTrackMarker(size, distance, lengthFactor);
+
+      expect(marker.dx, closeTo((1 - sin(rad)) * r + THICK + offset.dx, 1e-6));
+      expect(marker.dy, closeTo((cos(rad) + 1) * r + THICK + offset.dy, 1e-6));
+    });
+  });
+
+  test(
+      'calculateTrackMarker on the second (bottom) straight is placed proportionally',
+      () async {
+    final rnd = Random();
+    final count = rnd.nextInt(99) + 1;
+    getRandomDoubles(count, 2, rnd).forEach((lengthFactor) {
+      trackMap["Painting"] = TrackDescriptor(
+        radiusBoost: 1 + rnd.nextDouble() / 3,
+        lengthFactor: lengthFactor,
+      );
+      final track = trackMap["Painting"];
+      final size = Size(minPixel + rnd.nextDouble() * maxPixel,
+          minPixel + rnd.nextDouble() * maxPixel);
+      RecordingState.trackSize = size;
+      final rX = (size.width - 2 * THICK) /
+          (2 * track.radiusBoost + pi * track.laneShrink);
+      final rY = (size.height - 2 * THICK) / (2 * track.radiusBoost);
+      final r = min(rY, rX) * track.radiusBoost;
+      RecordingState.trackRadius = r;
+      final offset = Offset(
+          rX < rY
+              ? 0
+              : (size.width - 2 * (THICK + r) - pi * r * track.laneShrink) / 2,
+          rX > rY ? 0 : (size.height - 2 * (THICK + r)) / 2);
+      RecordingState.trackOffset = offset;
+      final laps = rnd.nextInt(100);
+      final positionRatio = rnd.nextDouble();
+      final distance = (laps + 0.5) * TRACK_LENGTH * lengthFactor +
+          positionRatio * track.laneLength;
+      final trackLength = TRACK_LENGTH * lengthFactor;
+      final d = distance % trackLength;
+      final displacement = (d - trackLength / 2) /
+          track.laneLength *
+          pi *
+          track.laneShrink /
+          track.radiusBoost *
+          r;
+
+      final marker = calculateTrackMarker(size, distance, lengthFactor);
+
+      expect(marker.dx, closeTo(THICK + offset.dx + r + displacement, 1e-6));
+      expect(marker.dy, closeTo(size.height - THICK - offset.dy, 1e-6));
+    });
+  });
+
+  test(
+      'calculateTrackMarker on the second (right) chicane is placed proportionally',
+      () async {
+    final rnd = Random();
+    final count = rnd.nextInt(99) + 1;
+    getRandomDoubles(count, 2, rnd).forEach((lengthFactor) {
+      trackMap["Painting"] = TrackDescriptor(
+        radiusBoost: 1 + rnd.nextDouble() / 3,
+        lengthFactor: lengthFactor,
+      );
+      final track = trackMap["Painting"];
+      final size = Size(minPixel + rnd.nextDouble() * maxPixel,
+          minPixel + rnd.nextDouble() * maxPixel);
+      RecordingState.trackSize = size;
+      final rX = (size.width - 2 * THICK) /
+          (2 * track.radiusBoost + pi * track.laneShrink);
+      final rY = (size.height - 2 * THICK) / (2 * track.radiusBoost);
+      final r = min(rY, rX) * track.radiusBoost;
+      RecordingState.trackRadius = r;
+      final offset = Offset(
+          rX < rY
+              ? 0
+              : (size.width - 2 * (THICK + r) - pi * r * track.laneShrink) / 2,
+          rX > rY ? 0 : (size.height - 2 * (THICK + r)) / 2);
+      RecordingState.trackOffset = offset;
+      final laps = rnd.nextInt(100);
+      final positionRatio = rnd.nextDouble();
+      final distance = (laps + 0.5) * TRACK_LENGTH * lengthFactor +
+          track.laneLength +
+          positionRatio * track.halfCircle;
+      final trackLength = TRACK_LENGTH * lengthFactor;
+      final d = distance % trackLength;
+      final rad =
+          (2 + (d - trackLength / 2 - track.laneLength) / track.halfCircle) *
+              pi;
+
+      final marker = calculateTrackMarker(size, distance, lengthFactor);
+
+      expect(marker.dx,
+          closeTo(size.width - THICK - offset.dx - (1 - sin(rad)) * r, 1e-6));
+      expect(marker.dy, closeTo(r * (cos(rad) + 1) + THICK + offset.dy, 1e-6));
     });
   });
 }
