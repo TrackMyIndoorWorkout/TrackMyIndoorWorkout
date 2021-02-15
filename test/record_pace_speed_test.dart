2bc549f65881c55f53374affb1150e0042f0df41
MrCsabaToth <csaba.toth.us@gmail.com>
2021-02-15 11:09:40 -0800

    #73 Adding test for paddling pace to speed conversion during Record construction

--- test/record_pace_speed_test.dart
+++ test/record_pace_speed_test.dart
@@ -52,4 +52,42 @@ void main() {
       });
     });
   });
+
+  group("Record constructor fills speed properly if paddling pace is present:", () {
+    final paces = [
+      [0.0, 0.0],
+      [120.0, 15.0],
+      [132.0, 13.636363637],
+      [150.0, 12.0],
+      [180.0, 10.0],
+      [192.0, 9.375],
+      [210.0, 8.571428572],
+      [240.0, 7.5],
+      [270.0, 6.666666667],
+      [288.0, 6.25],
+      [300.0, 6.0],
+      [360.0, 5.0],
+    ];
+    paces.forEach((pacePair) {
+      final expected = pacePair[1];
+      [ActivityType.Kayaking, ActivityType.Canoeing, ActivityType.Rowing].forEach((sport) {
+        test("${pacePair[0]} -> $expected", () {
+          expect(Record(pace: pacePair[0], sport: sport).speed, closeTo(expected, 1e-6));
+        });
+      });
+    });
+  });
+
+  group("Record constructor fills speed properly if random paddling pace is present:", () {
+    final rnd = Random();
+    1.to(REPETITION).forEach((input) {
+      final randomPace = rnd.nextDouble() * 360;
+      final expected = 30.0 / (randomPace / 60.0);
+      [ActivityType.Kayaking, ActivityType.Canoeing, ActivityType.Rowing].forEach((sport) {
+        test("$randomPace -> $expected", () {
+          expect(Record(pace: randomPace, sport: sport).speed, closeTo(expected, 1e-6));
+        });
+      });
+    });
+  });
 }
