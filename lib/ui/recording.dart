fe9da6429e92c0eaaa71037241b9ed2959dec856
MrCsabaToth <csaba.toth.us@gmail.com>
2021-03-18 03:37:01 -0700

    Correct HRM kick-in logic: attach after discover and not the other way around

--- lib/ui/recording.dart
+++ lib/ui/recording.dart
@@ -334,16 +334,16 @@ class RecordingState extends State<RecordingScreen> {
     _distance = 0.0;
     _elapsed = 0;
 
-    _heartRateMonitor?.attach()?.then((_) {
-      _heartRateMonitor?.discover()?.then((discovered) {
-        if (discovered) {
+    _heartRateMonitor?.discover()?.then((discovered) {
+      if (discovered) {
+        _heartRateMonitor?.attach()?.then((_) {
           _heartRateMonitor?.pumpMetric((heartRate) async {
             setState(() {
               _values[4] = heartRate?.toString() ?? "--";
             });
           });
-        }
-      });
+        });
+      }
     });
     _connectOnDemand(initialState);
     _openDatabase();
