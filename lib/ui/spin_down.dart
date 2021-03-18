701a28ec556bc97432151c5547e0fc8326f423b9
MrCsabaToth <csaba.toth.us@gmail.com>
2021-03-18 07:39:51 -0700

    Remove battery level display from spin down #75

--- lib/ui/spin_down.dart
+++ lib/ui/spin_down.dart
@@ -4,34 +4,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 import 'package:get/get.dart';
 import 'package:preferences/preferences.dart';
-import 'package:progress_indicators/progress_indicators.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:spinner_input/spinner_input.dart';
-import '../devices/device_descriptors/device_descriptor.dart';
 import '../devices/device_descriptors/fitness_machine_descriptor.dart';
-import '../devices/gadgets/heart_rate_monitor.dart';
+import '../devices/gadgets/fitness_equipment.dart';
 import '../devices/bluetooth_device_ex.dart';
 import '../devices/gatt_constants.dart';
 import '../persistence/preferences.dart';
 import '../utils/constants.dart';
 
 class SpinDownBottomSheet extends StatefulWidget {
-  final BluetoothDevice device;
-  final DeviceDescriptor descriptor;
-
-  SpinDownBottomSheet({
-    Key key,
-    @required this.device,
-    @required this.descriptor,
-  })  : assert(device != null),
-        assert(descriptor != null),
-        super(key: key);
-
   @override
-  _SpinDownBottomSheetState createState() => _SpinDownBottomSheetState(
-        device: device,
-        descriptor: descriptor,
-      );
+  _SpinDownBottomSheetState createState() => _SpinDownBottomSheetState();
 }
 
 enum CalibrationState {
@@ -55,11 +39,7 @@ class _SpinDownBottomSheetState extends State<SpinDownBottomSheet> {
   static const STEP_DONE = 2;
   static const STEP_NOT_SUPPORTED = 3;
 
-  final BluetoothDevice device;
-  final DeviceDescriptor descriptor;
-  HeartRateMonitor _heartRateMonitor;
-  String _hrmBatteryLevel;
-  String _batteryLevel;
+  FitnessEquipment _fitnessEquipment;
   double _sizeDefault;
   TextStyle _smallerTextStyle;
   TextStyle _largerTextStyle;
@@ -86,71 +66,45 @@ class _SpinDownBottomSheetState extends State<SpinDownBottomSheet> {
   bool get _canSubmitWeight =>
       _spinDownPossible && _calibrationState == CalibrationState.ReadyToWeighIn;
 
-  _SpinDownBottomSheetState({
-    @required this.device,
-    @required this.descriptor,
-  }) : assert(device != null);
+  Future<bool> _prepareSpinDownCore() async {
+    if (_fitnessEquipment?.device == null) return false;
 
-  _prepareSpinDown(List<BluetoothService> services) async {
-    final userData = BluetoothDeviceEx.filterService(services, USER_DATA_SERVICE);
+    if (!_fitnessEquipment.connected) {
+      await _fitnessEquipment.connect();
+    }
+
+    if (!_fitnessEquipment.connected) return false;
+
+    if (!_fitnessEquipment.discovered) {
+      await _fitnessEquipment.discover();
+    }
+
+    if (!_fitnessEquipment.discovered) return false;
+
+    final userData = BluetoothDeviceEx.filterService(_fitnessEquipment.services, USER_DATA_SERVICE);
     _weightData =
         BluetoothDeviceEx.filterCharacteristic(userData?.characteristics, WEIGHT_CHARACTERISTIC);
-    final fitnessMachine = BluetoothDeviceEx.filterService(services, FITNESS_MACHINE_ID);
+    final fitnessMachine =
+        BluetoothDeviceEx.filterService(_fitnessEquipment.services, FITNESS_MACHINE_ID);
     _controlPoint = BluetoothDeviceEx.filterCharacteristic(
         fitnessMachine?.characteristics, FITNESS_MACHINE_CONTROL_POINT);
     _fitnessMachineStatus = BluetoothDeviceEx.filterCharacteristic(
         fitnessMachine?.characteristics, FITNESS_MACHINE_STATUS);
-    final dataService = descriptor.isFitnessMachine
-        ? fitnessMachine
-        : BluetoothDeviceEx.filterService(services, descriptor.dataServiceId);
-    _fitnessMachineData = BluetoothDeviceEx.filterCharacteristic(dataService?.characteristics,
-        (descriptor as FitnessMachineDescriptor).dataCharacteristicId);
+    _fitnessMachineData = BluetoothDeviceEx.filterCharacteristic(fitnessMachine?.characteristics,
+        (_fitnessEquipment.descriptor as FitnessMachineDescriptor).dataCharacteristicId);
+
+    return _spinDownPossible;
+  }
+
+  _prepareSpinDown() async {
+    final success = await _prepareSpinDownCore();
     setState(() {
-      if (!_spinDownPossible) {
+      if (!success) {
         _step = STEP_NOT_SUPPORTED;
         _calibrationState = CalibrationState.NotSupported;
       } else {
         _calibrationState = CalibrationState.ReadyToWeighIn;
       }
-    });
-  }
-
-  Future<String> _readBatteryLevel(List<BluetoothService> services) async {
-    final batteryService = BluetoothDeviceEx.filterService(services, BATTERY_SERVICE_ID);
-    if (batteryService == null) {
-      return "N/A";
-    }
-    final batteryLevel =
-        BluetoothDeviceEx.filterCharacteristic(batteryService.characteristics, BATTERY_LEVEL_ID);
-    if (batteryLevel == null) {
-      return "N/A";
-    }
-    final batteryLevelData = await batteryLevel.read();
-    return "${batteryLevelData[0]}%";
-  }
-
-  _readBatteryLevels() async {
-    setState(() {
-      _calibrationState = CalibrationState.Initializing;
-    });
-    if (_heartRateMonitor?.connected ?? false) {
-      _heartRateMonitor.device.discoverServices().then((services) async {
-        final batteryLevel = await _readBatteryLevel(services);
-        setState(() {
-          _hrmBatteryLevel = batteryLevel;
-        });
-      });
-    } else {
-      setState(() {
-        _hrmBatteryLevel = "N/A";
-      });
-    }
-    device.discoverServices().then((services) async {
-      await _prepareSpinDown(services);
-      final batteryLevel = await _readBatteryLevel(services);
-      setState(() {
-        _batteryLevel = batteryLevel;
-      });
     });
   }
 
@@ -344,8 +298,7 @@ class _SpinDownBottomSheetState extends State<SpinDownBottomSheet> {
   @override
   initState() {
     super.initState();
-    _hrmBatteryLevel = "N/A";
-    _batteryLevel = "N/A";
+    _fitnessEquipment = Get.isRegistered<FitnessEquipment>() ? Get.find<FitnessEquipment>() : null;
     _step = STEP_WEIGHT_INPUT;
     _calibrationState = CalibrationState.PreInit;
     _targetSpeedHigh = 0.0;
@@ -357,8 +310,7 @@ class _SpinDownBottomSheetState extends State<SpinDownBottomSheet> {
     _largerTextStyle = TextStyle(fontFamily: FONT_FAMILY, fontSize: _sizeDefault * 2);
     _si = PrefService.getBool(UNIT_SYSTEM_TAG);
     _weight = _si ? 60 : 130;
-    _heartRateMonitor = Get.isRegistered<HeartRateMonitor>() ? Get.find<HeartRateMonitor>() : null;
-    _readBatteryLevels();
+    _prepareSpinDown();
   }
 
   @override
@@ -378,138 +330,112 @@ class _SpinDownBottomSheetState extends State<SpinDownBottomSheet> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Row(
+      body: IndexedStack(
+        index: _step,
+        children: <Widget>[
+          // 0 - STEP_WEIGHT_INPUT
+          Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              _hrmBatteryLevel == null
-                  ? JumpingDotsProgressIndicator(fontSize: _sizeDefault)
-                  : Row(children: [
-                      Icon(Icons.battery_unknown, color: Colors.indigo, size: _sizeDefault),
-                      Text(_batteryLevel, style: _smallerTextStyle),
-                    ]),
-              _hrmBatteryLevel == null
-                  ? JumpingDotsProgressIndicator(fontSize: _sizeDefault)
-                  : Row(children: [
-                      Icon(Icons.favorite, color: Colors.indigo, size: _sizeDefault),
-                      Icon(Icons.battery_unknown, color: Colors.indigo, size: _sizeDefault),
-                      Text(_hrmBatteryLevel, style: _smallerTextStyle),
-                    ]),
-            ],
-          ),
-          IndexedStack(
-            index: _step,
-            children: <Widget>[
-              // 0 - STEP_WEIGHT_INPUT
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text("Weight (${_si ? "kg" : "lbs"}):", style: _smallerTextStyle),
-                  SpinnerInput(
-                    spinnerValue: _weight.toDouble(),
-                    minValue: 1,
-                    maxValue: 800,
-                    middleNumberStyle: _largerTextStyle,
-                    plusButton: SpinnerButtonStyle(
-                      height: _sizeDefault * 2,
-                      width: _sizeDefault * 2,
-                      child: Icon(Icons.add, size: _sizeDefault * 2 - 10),
-                    ),
-                    minusButton: SpinnerButtonStyle(
-                      height: _sizeDefault * 2,
-                      width: _sizeDefault * 2,
-                      child: Icon(Icons.remove, size: _sizeDefault * 2 - 10),
-                    ),
-                    onChange: (newValue) {
-                      setState(() {
-                        _weight = newValue.toInt();
-                      });
-                    },
-                  ),
-                  Center(
-                    child: ElevatedButton(
-                      child: Text(
-                        _weightInputButtonText(),
-                        style: _weightInputButtonTextStyle(),
-                      ),
-                      style: _weightInputButtonStyle(),
-                      onPressed: () async => await _onWeightInputButtonPressed(),
-                    ),
-                  ),
-                ],
+              Text("Weight (${_si ? "kg" : "lbs"}):", style: _smallerTextStyle),
+              SpinnerInput(
+                spinnerValue: _weight.toDouble(),
+                minValue: 1,
+                maxValue: 800,
+                middleNumberStyle: _largerTextStyle,
+                plusButton: SpinnerButtonStyle(
+                  height: _sizeDefault * 2,
+                  width: _sizeDefault * 2,
+                  child: Icon(Icons.add, size: _sizeDefault * 2 - 10),
+                ),
+                minusButton: SpinnerButtonStyle(
+                  height: _sizeDefault * 2,
+                  width: _sizeDefault * 2,
+                  child: Icon(Icons.remove, size: _sizeDefault * 2 - 10),
+                ),
+                onChange: (newValue) {
+                  setState(() {
+                    _weight = newValue.toInt();
+                  });
+                },
               ),
-              // 1 - STEP_CALIBRATING
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text("$_targetSpeedLow", style: _smallerTextStyle),
-                      Icon(Icons.multiple_stop, size: _sizeDefault),
-                      Text("$_targetSpeedHigh", style: _smallerTextStyle),
-                      Text("km/h", style: _smallerTextStyle),
-                    ],
-                  ),
-                  Text("$_currentSpeed",
-                      style: _largerTextStyle.merge(TextStyle(color: Colors.indigo))),
-                  Text(_calibrationInstruction(), style: _calibrationInstructionStyle()),
-                  Center(
-                    child: ElevatedButton(
-                      child: Text(_calibrationButtonText(), style: _smallerTextStyle),
-                      onPressed: () async => await onCalibrationButtonPressed(),
-                    ),
-                  ),
-                ],
-              ),
-              // 2 - STEP_END
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                      _calibrationState == CalibrationState.CalibrationSuccess
-                          ? "SUCCESS"
-                          : "ERROR",
-                      style: _largerTextStyle),
-                  Row(
-                    children: [
-                      ElevatedButton(
-                        child: Text(
-                            _calibrationState == CalibrationState.CalibrationSuccess
-                                ? 'Close'
-                                : 'Retry',
-                            style: _smallerTextStyle),
-                        onPressed: () {
-                          if (_calibrationState == CalibrationState.CalibrationSuccess) {
-                            Get.close(1);
-                          } else {
-                            _step = STEP_WEIGHT_INPUT;
-                            _calibrationState = CalibrationState.ReadyToWeighIn;
-                          }
-                        },
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              // 3 - STEP_NOT_SUPPORTED
               Center(
-                child: RichText(
-                  textAlign: TextAlign.center,
-                  softWrap: true,
-                  text: TextSpan(
-                    text: "${device.name} doesn't seem to support calibration",
-                    style: _smallerTextStyle,
+                child: ElevatedButton(
+                  child: Text(
+                    _weightInputButtonText(),
+                    style: _weightInputButtonTextStyle(),
                   ),
+                  style: _weightInputButtonStyle(),
+                  onPressed: () async => await _onWeightInputButtonPressed(),
                 ),
               ),
             ],
+          ),
+          // 1 - STEP_CALIBRATING
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text("$_targetSpeedLow", style: _smallerTextStyle),
+                  Icon(Icons.multiple_stop, size: _sizeDefault),
+                  Text("$_targetSpeedHigh", style: _smallerTextStyle),
+                  Text("km/h", style: _smallerTextStyle),
+                ],
+              ),
+              Text("$_currentSpeed",
+                  style: _largerTextStyle.merge(TextStyle(color: Colors.indigo))),
+              Text(_calibrationInstruction(), style: _calibrationInstructionStyle()),
+              Center(
+                child: ElevatedButton(
+                  child: Text(_calibrationButtonText(), style: _smallerTextStyle),
+                  onPressed: () async => await onCalibrationButtonPressed(),
+                ),
+              ),
+            ],
+          ),
+          // 2 - STEP_END
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(_calibrationState == CalibrationState.CalibrationSuccess ? "SUCCESS" : "ERROR",
+                  style: _largerTextStyle),
+              Row(
+                children: [
+                  ElevatedButton(
+                    child: Text(
+                        _calibrationState == CalibrationState.CalibrationSuccess
+                            ? 'Close'
+                            : 'Retry',
+                        style: _smallerTextStyle),
+                    onPressed: () {
+                      if (_calibrationState == CalibrationState.CalibrationSuccess) {
+                        Get.close(1);
+                      } else {
+                        _step = STEP_WEIGHT_INPUT;
+                        _calibrationState = CalibrationState.ReadyToWeighIn;
+                      }
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+          // 3 - STEP_NOT_SUPPORTED
+          Center(
+            child: RichText(
+              textAlign: TextAlign.center,
+              softWrap: true,
+              text: TextSpan(
+                text: "${_fitnessEquipment.device.name} doesn't seem to support calibration",
+                style: _smallerTextStyle,
+              ),
+            ),
           ),
         ],
       ),
