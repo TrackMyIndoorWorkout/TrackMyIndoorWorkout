17850f4007b0142510a4899df755932417dd29bd
MrCsabaToth <csaba.toth.us@gmail.com>
2021-03-19 13:00:41 -0700

    Change silly import

--- lib/ui/scan_result.dart
+++ lib/ui/scan_result.dart
@@ -1,4 +1,3 @@
-import 'package:charts_flutter/flutter.dart' hide TextStyle;
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 import '../devices/device_map.dart';
