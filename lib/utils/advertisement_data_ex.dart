298cdfbc2bc49aa62341b3fd9fc3892de51e376a
MrCsabaToth <csaba.toth.us@gmail.com>
2021-04-02 13:10:52 -0700

    #115 Flutter format

--- lib/utils/advertisement_data_ex.dart
+++ lib/utils/advertisement_data_ex.dart
@@ -2,5 +2,6 @@ import 'package:flutter_blue/flutter_blue.dart';
 import 'string_ex.dart';
 
 extension AdvertisementDataEx on AdvertisementData {
-  List<String> get uuids => serviceUuids.isEmpty ? [] : serviceUuids.map((x) => x.uuidString()).toList();
+  List<String> get uuids =>
+      serviceUuids.isEmpty ? [] : serviceUuids.map((x) => x.uuidString()).toList();
 }
