8307c1248a1c5dc017eff71839927d6e80b0d5ba
MrCsabaToth <csaba.toth.us@gmail.com>
2021-04-21 23:14:24 -0700

    Tuning FIT sports #92

--- lib/export/fit/fit_sport.dart
+++ lib/export/fit/fit_sport.dart
@@ -2,26 +2,31 @@ import 'package:tuple/tuple.dart';
 
 const FITNESS_EQUIPMENT_SPORT_ID = 4;
 const GENERIC_SPORT_ID = 0;
-const PADDLING_SPORT_ID = 19;
 const RUNNING_SPORT_ID = 1;
+const CYCLING_SPORT_ID = 2;
+const SWIMMING_SPORT_ID = 5;
+const TRAINING_SPORT_ID = 10;
+const PADDLING_SPORT_ID = 19;
 
 Map<String, Tuple2<int, int>> fitSport = {
   "AlpineSki": Tuple2(13, 9),
   "BackcountrySki": Tuple2(12, 0), // Crosscountry skiing
   "Canoeing": Tuple2(PADDLING_SPORT_ID, 0), // Paddling
-  "Crossfit": Tuple2(10, 0), // Training
-  "EBikeRide": Tuple2(GENERIC_SPORT_ID, 0),
+  "Crossfit": Tuple2(TRAINING_SPORT_ID, 0), // Training
+  "EBikeRide": Tuple2(CYCLING_SPORT_ID, 28),
   "Elliptical": Tuple2(FITNESS_EQUIPMENT_SPORT_ID, 15), // Fitness Equipment, Elliptical
   "Golf": Tuple2(25, 0),
-  "Handcycle": Tuple2(GENERIC_SPORT_ID, 0),
+  "Handcycle": Tuple2(GENERIC_SPORT_ID, 12),
   "Hike": Tuple2(17, 3), // Hiking, Trail
   "IceSkate": Tuple2(33, 0),
   "IndoorRowing": Tuple2(FITNESS_EQUIPMENT_SPORT_ID, 14),
-  "InlineSkate": Tuple2(GENERIC_SPORT_ID, 0),
-  "Kayaking": Tuple2(PADDLING_SPORT_ID, 0),
+  "IndoorRunning": Tuple2(RUNNING_SPORT_ID, 45),
+  "InlineSkate": Tuple2(30, 0),
+  "Kayaking": Tuple2(41, 0),
   "Kitesurf": Tuple2(44, 0),
   "NordicSki": Tuple2(GENERIC_SPORT_ID, 0),
-  "Ride": Tuple2(2, 0), // Cycling
+  "OpenWaterSwim": Tuple2(SWIMMING_SPORT_ID, 18),
+  "Ride": Tuple2(CYCLING_SPORT_ID, 0), // Cycling
   "RockClimbing": Tuple2(48, 0), // Floor climbing
   "RollerSki": Tuple2(GENERIC_SPORT_ID, 0),
   "Rowing": Tuple2(15, 0),
@@ -31,18 +36,21 @@ Map<String, Tuple2<int, int>> fitSport = {
   "Snowboard": Tuple2(14, 0),
   "Snowshoe": Tuple2(35, 0),
   "Soccer": Tuple2(7, 0),
-  "StairStepper": Tuple2(GENERIC_SPORT_ID, 0),
+  "StairStepper": Tuple2(FITNESS_EQUIPMENT_SPORT_ID, 16),
   "StandUpPaddling": Tuple2(37, 0),
   "Surfing": Tuple2(38, 0),
-  "Swim": Tuple2(5, 0),
+  "Swim": Tuple2(SWIMMING_SPORT_ID, 0),
   "Treadmill": Tuple2(RUNNING_SPORT_ID, 1), // Treadmill running
+  "TrackRide": Tuple2(CYCLING_SPORT_ID, 13),
+  "TrackRun": Tuple2(RUNNING_SPORT_ID, 4),
   "Velomobile": Tuple2(GENERIC_SPORT_ID, 0),
-  "VirtualRide": Tuple2(2, 6), // Cycling, Indoor Cycling
+  "VirtualRide": Tuple2(CYCLING_SPORT_ID, 6), // Cycling, Indoor Cycling
   "VirtualRun": Tuple2(RUNNING_SPORT_ID, 1), // Treadmill running
+  "VirtualRowing": Tuple2(PADDLING_SPORT_ID, 14), // Indoor Rowing
   "Walk": Tuple2(11, 0),
-  "WeightTraining": Tuple2(10, 0), // Training
+  "WeightTraining": Tuple2(TRAINING_SPORT_ID, 0), // Training
   "Wheelchair": Tuple2(GENERIC_SPORT_ID, 0),
   "Windsurf": Tuple2(43, 0),
   "Workout": Tuple2(GENERIC_SPORT_ID, 0),
-  "Yoga": Tuple2(0, 0),
+  "Yoga": Tuple2(TRAINING_SPORT_ID, 43),
 };
