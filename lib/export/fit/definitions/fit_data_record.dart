c19a9b904dee8a15c4ac3764d01bd2858f18f485
MrCsabaToth <csaba.toth.us@gmail.com>
2021-04-22 21:10:25 -0700

    Record data serialization without data gap workaround code #92

--- lib/export/fit/definitions/fit_data_record.dart
+++ lib/export/fit/definitions/fit_data_record.dart
@@ -1,7 +1,11 @@
+import '../../../utils/constants.dart';
+import '../../export_record.dart';
 import '../fit_base_type.dart';
 import '../fit_definition_message.dart';
 import '../fit_field.dart';
+import '../fit_header.dart';
 import '../fit_message.dart';
+import '../fit_serializable.dart';
 
 class FitDataRecord extends FitDefinitionMessage {
   FitDataRecord({localMessageType})
@@ -15,10 +19,30 @@ class FitDataRecord extends FitDefinitionMessage {
       FitField(1, FitBaseTypes.sint32Type), // PositionLong
       FitField(3, FitBaseTypes.uint8Type), // HeartRate (bpm)
       FitField(4, FitBaseTypes.uint8Type), // Cadence (rpm or spm?)
-      FitField(5, FitBaseTypes.uint32Type), // Distance (1/00 m)
+      FitField(5, FitBaseTypes.uint32Type), // Distance (1/100 m)
       FitField(6, FitBaseTypes.uint16Type), // Speed (1/1000 m/s)
       FitField(7, FitBaseTypes.uint16Type), // Power (Watts)
-      FitField(33, FitBaseTypes.uint16Type), // Calories
     ];
+  }
+
+  List<int> serializeData(dynamic parameter) {
+    ExportRecord model = parameter;
+
+    var dummy = FitHeader();
+    dummy.output = [localMessageType];
+    dummy.addLong(FitSerializable.fitDateTime(model.date));
+    dummy.addLong((model.latitude * DEG_TO_FIT_GPS).round());
+    dummy.addLong((model.longitude * DEG_TO_FIT_GPS).round());
+    // TODO
+    dummy.addByte(model.heartRate);
+    // TODO
+    dummy.addByte(model.cadence);
+    dummy.addLong((model.distance * 100).round());
+    dummy.addShort((model.speed * 1000).round());
+    dummy.addShort(model.power.round());
+
+    dummy.output.addAll([1, 0, 0x1A, 1]);
+
+    return dummy.output;
   }
 }
