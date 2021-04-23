8fdaa4b4d836af7cfb7283acbe29f5366951e434
MrCsabaToth <csaba.toth.us@gmail.com>
2021-04-22 21:10:46 -0700

    FitActivity serialization #92

--- lib/export/fit/definitions/fit_activity.dart
+++ lib/export/fit/definitions/fit_activity.dart
@@ -1,7 +1,10 @@
+import '../../export_model.dart';
 import '../fit_base_type.dart';
 import '../fit_definition_message.dart';
 import '../fit_field.dart';
+import '../fit_header.dart';
 import '../fit_message.dart';
+import '../fit_serializable.dart';
 
 class FitActivity extends FitDefinitionMessage {
   FitActivity({localMessageType})
@@ -11,12 +14,21 @@ class FitActivity extends FitDefinitionMessage {
         ) {
     fields = [
       FitField(253, FitBaseTypes.uint32Type), // Timestamp
-      // FitField(0, FitBaseTypes.uint32Type), // TotalTimerTime (exclude pauses) 1/1000 s
       FitField(1, FitBaseTypes.uint16Type), // NumSessions: 1
       FitField(2, FitBaseTypes.enumType), // Timestamp or Activity?: 0
       FitField(3, FitBaseTypes.enumType), // Event: 1A
       FitField(4, FitBaseTypes.enumType), // EventType: 1
-      // FitField(5, FitBaseTypes.uint32Type), // LocalTimestamp
     ];
+  }
+
+  List<int> serializeData(dynamic parameter) {
+    ExportModel model = parameter;
+
+    var dummy = FitHeader();
+    dummy.output = [localMessageType];
+    dummy.addLong(FitSerializable.fitDateTime(model.dateActivity));
+    dummy.output.addAll([1, 0, 0x1A, 1]);
+
+    return dummy.output;
   }
 }
