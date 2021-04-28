05c70185a0268f9239d891e7ea686934b25c401b
MrCsabaToth <csaba.toth.us@gmail.com>
2021-04-27 17:49:30 -0700

    Focusing String serialization test on the field definition (so length comes through) #92

--- test/fit_definition_message_test.dart
+++ test/fit_definition_message_test.dart
@@ -30,8 +30,14 @@ class FitStringFieldTest extends FitDefinitionMessage {
   final String text;
   final int byte2;
 
-  FitStringFieldTest({localMessageType, globalMessageNumber, this.definitionNumber, this.byte1, this.text, this.byte2,})
-      : super(
+  FitStringFieldTest({
+    localMessageType,
+    globalMessageNumber,
+    this.definitionNumber,
+    this.byte1,
+    this.text,
+    this.byte2,
+  }) : super(
           localMessageType: localMessageType,
           globalMessageNumber: globalMessageNumber,
         ) {
@@ -70,9 +76,10 @@ void main() {
         0
       ];
       test('$short -> $expected', () async {
-        final subject =
-            FitDefinitionMessageTest(localMessageType: localMessageType, globalMessageNumber: short,)
-              ..architecture = bigEndian ? 1 : 0;
+        final subject = FitDefinitionMessageTest(
+          localMessageType: localMessageType,
+          globalMessageNumber: short,
+        )..architecture = bigEndian ? 1 : 0;
 
         final output = subject.serializeData(null);
 
@@ -90,14 +97,66 @@ void main() {
       final localMessageType = rnd.nextInt(6);
       final globalMessageNumber = rnd.nextInt(MAX_UINT16);
       final definitionNumber = rnd.nextInt(6);
-      final expected =  [localMessageType, 0, byte1] + utf8.encode(text) + [0, byte2];
+      final expected = [localMessageType, 0, byte1] + utf8.encode(text) + [0, byte2];
       test('$text($length) -> $expected', () async {
-        final subject =
-        FitStringFieldTest(localMessageType: localMessageType, globalMessageNumber: globalMessageNumber, definitionNumber: definitionNumber, byte1: byte1, text: text, byte2: byte2,);
+        final subject = FitStringFieldTest(
+          localMessageType: localMessageType,
+          globalMessageNumber: globalMessageNumber,
+          definitionNumber: definitionNumber,
+          byte1: byte1,
+          text: text,
+          byte2: byte2,
+        );
 
         final output = subject.serializeData(null);
 
         expect(listEquals(output, expected), true);
+      });
+    });
+
+    group('FitDefinition strings with headers serialize properly', () {
+      final rnd = Random();
+      getRandomInts(SMALL_REPETITION, MAX_UINT8 ~/ 8, rnd).forEach((length) {
+        final byte1 = rnd.nextInt(MAX_BYTE);
+        final text = mockString(length + 1);
+        final byte2 = rnd.nextInt(MAX_BYTE);
+        final localMessageType = rnd.nextInt(6);
+        final globalMessageNumber = rnd.nextInt(MAX_UINT16);
+        final definitionNumber = rnd.nextInt(6);
+        final shortLsb = globalMessageNumber % MAX_UINT8;
+        final shortMsb = globalMessageNumber ~/ MAX_UINT8;
+        final expected = [
+          FitDefinitionMessage.FORTY_RECORD + localMessageType,
+          0,
+          0,
+          shortLsb,
+          shortMsb,
+          3,
+          definitionNumber,
+          FitBaseTypes.uint8Type.size,
+          FitBaseTypes.uint8Type.id,
+          definitionNumber + 1,
+          text.length + 1,
+          FitBaseTypes.stringType.id,
+          definitionNumber + 2,
+          FitBaseTypes.uint8Type.size,
+          FitBaseTypes.uint8Type.id,
+        ];
+        test('$text($length) -> $expected', () async {
+          final subject = FitStringFieldTest(
+            localMessageType: localMessageType,
+            globalMessageNumber: globalMessageNumber,
+            definitionNumber: definitionNumber,
+            byte1: byte1,
+            text: text,
+            byte2: byte2,
+          );
+          subject.setStringFieldSize(definitionNumber + 1, text.length + 1);
+
+          final output = subject.binarySerialize();
+
+          expect(listEquals(output, expected), true);
+        });
       });
     });
   });
