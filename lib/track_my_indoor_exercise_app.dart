b31cc291278a0e9a45dbc6e7284cde16c61630cf
MrCsabaToth <csaba.toth.us@gmail.com>
2021-04-28 23:30:00 -0700

    Trying to correct #120 - introduce package specifically to explicitly ask for Bluetooth enable and permission

--- lib/track_my_indoor_exercise_app.dart
+++ lib/track_my_indoor_exercise_app.dart
@@ -1,10 +1,30 @@
+import 'package:bluetooth_enable/bluetooth_enable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 import 'package:get/get.dart';
 import 'ui/bluetooth_off.dart';
 import 'ui/find_devices.dart';
 
-class TrackMyIndoorExerciseApp extends StatelessWidget {
+class TrackMyIndoorExerciseApp extends StatefulWidget {
+  TrackMyIndoorExerciseApp({key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return TrackMyIndoorExerciseAppState();
+  }
+}
+
+class TrackMyIndoorExerciseAppState extends State<TrackMyIndoorExerciseApp> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      BluetoothEnable.enableBluetooth.then((result) {
+        debugPrint(result);
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
