34612e8dbaad7ed55d8b3ea81a98ab16ac9b45f6
MrCsabaToth <csaba.toth.us@gmail.com>
2021-05-01 00:27:18 -0700

    Move bluetooth enable plugin to BluetoothOff screen. We'll need another plugin to attack the white screen of #120

--- lib/track_my_indoor_exercise_app.dart
+++ lib/track_my_indoor_exercise_app.dart
@@ -1,30 +1,10 @@
-import 'package:bluetooth_enable/bluetooth_enable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 import 'package:get/get.dart';
 import 'ui/bluetooth_off.dart';
 import 'ui/find_devices.dart';
 
-class TrackMyIndoorExerciseApp extends StatefulWidget {
-  TrackMyIndoorExerciseApp({key}) : super(key: key);
-
-  @override
-  State<StatefulWidget> createState() {
-    return TrackMyIndoorExerciseAppState();
-  }
-}
-
-class TrackMyIndoorExerciseAppState extends State<TrackMyIndoorExerciseApp> {
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      BluetoothEnable.enableBluetooth.then((result) {
-        debugPrint(result);
-      });
-    });
-  }
-
+class TrackMyIndoorExerciseApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
--- lib/ui/bluetooth_off.dart
+++ lib/ui/bluetooth_off.dart
@@ -1,14 +1,32 @@
+import 'package:bluetooth_enable/bluetooth_enable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 
-class BluetoothOffScreen extends StatelessWidget {
-  const BluetoothOffScreen({
-    key,
-    @required this.state,
-  })  : assert(state != null),
-        super(key: key);
-
+class BluetoothOffScreen extends StatefulWidget {
   final BluetoothState state;
+
+  const BluetoothOffScreen({key, @required this.state}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return BluetoothOffScreenState(state: state);
+  }
+}
+
+class BluetoothOffScreenState extends State<BluetoothOffScreen> {
+  final BluetoothState state;
+
+  BluetoothOffScreenState({@required this.state}) : assert(state != null);
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      BluetoothEnable.enableBluetooth.then((result) {
+        debugPrint(result);
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
