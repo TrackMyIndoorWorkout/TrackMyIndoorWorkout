0b2869e0ae90f9d092abd01517edc1d46b9c038a
MrCsabaToth <csaba.toth.us@gmail.com>
2021-05-07 21:12:04 -0700

    Activity tuning initialization for #87, #88, #68

--- lib/ui/recording.dart
+++ lib/ui/recording.dart
@@ -145,7 +145,6 @@ class RecordingState extends State<RecordingScreen> {
 
   Future<void> _startMeasurement() async {
     final now = DateTime.now();
-    // TODO: factors
     _activity = Activity(
       fourCC: _descriptor.fourCC,
       deviceName: device.name,
@@ -153,6 +152,8 @@ class RecordingState extends State<RecordingScreen> {
       start: now.millisecondsSinceEpoch,
       startDateTime: now,
       sport: _descriptor.defaultSport,
+      powerFactor: await _database.powerFactor(device.id.id),
+      calorieFactor: await _database.calorieFactor(device.id.id, _descriptor),
     );
     if (!_uxDebug) {
       final id = await _database?.activityDao?.insertActivity(_activity);
