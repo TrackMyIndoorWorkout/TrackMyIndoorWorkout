f4f1fc90585fec9f184bb1d43e7c66c3cef4194a
MrCsabaToth <csaba.toth.us@gmail.com>
2021-05-11 13:51:37 -0700

    Breaking up preferences into sub pages #90

---
+++ lib/ui/preferences/expert.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:preferences/preferences.dart';
+import '../../persistence/preferences.dart';
+import '../../utils/preferences.dart';
+import 'preferences_base.dart';
+
+class ExpertPreferencesScreen extends PreferencesScreenBase {
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> expertPreferences = [
+      PreferenceTitle(EXPERT_PREFERENCES),
+      PreferenceTitle(DATA_CONNECTION_ADDRESSES_DESCRIPTION, style: descriptionStyle),
+      TextFieldPreference(
+        DATA_CONNECTION_ADDRESSES,
+        DATA_CONNECTION_ADDRESSES_TAG,
+        defaultVal: DATA_CONNECTION_ADDRESSES_DEFAULT,
+        validator: (str) {
+          final addressTuples = parseIpAddresses(str);
+          if (addressTuples == null || addressTuples.isEmpty) {
+            return "Invalid or empty addresses, default DNS servers will be used";
+          } else {
+            if (str.split(",").length > addressTuples.length) {
+              return "There's some malformed address(es) in the configuration";
+            }
+          }
+          return null;
+        },
+      ),
+    ];
+
+    if (kDebugMode) {
+      expertPreferences.add(SwitchPreference(
+        APP_DEBUG_MODE,
+        APP_DEBUG_MODE_TAG,
+        defaultVal: APP_DEBUG_MODE_DEFAULT,
+        desc: APP_DEBUG_MODE_DESCRIPTION,
+      ));
+    }
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Expert Prefs')),
+      body: PreferencePage(expertPreferences),
+    );
+  }
+}
---
+++ lib/ui/preferences/target_heart_rate.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:preferences/preferences.dart';
+import '../../persistence/preferences.dart';
+import '../../utils/preferences.dart';
+import 'preferences_base.dart';
+
+class MainPreferencesScreen extends PreferencesScreenBase {
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> targetHrPreferences = [
+      PreferenceTitle(TARGET_HEART_RATE_MODE_DESCRIPTION, style: descriptionStyle),
+      PreferenceDialogLink(
+        TARGET_HEART_RATE_MODE,
+        dialog: PreferenceDialog(
+          [
+            RadioPreference(
+              TARGET_HEART_RATE_MODE_NONE_DESCRIPTION,
+              TARGET_HEART_RATE_MODE_NONE,
+              TARGET_HEART_RATE_MODE_TAG,
+            ),
+            RadioPreference(
+              TARGET_HEART_RATE_MODE_BPM_DESCRIPTION,
+              TARGET_HEART_RATE_MODE_BPM,
+              TARGET_HEART_RATE_MODE_TAG,
+            ),
+            RadioPreference(
+              TARGET_HEART_RATE_MODE_ZONES_DESCRIPTION,
+              TARGET_HEART_RATE_MODE_ZONES,
+              TARGET_HEART_RATE_MODE_TAG,
+            ),
+          ],
+          title: 'Select Target HR Method',
+          cancelText: 'Close',
+        ),
+      ),
+      TextFieldPreference(
+        TARGET_HEART_RATE_LOWER_BPM,
+        TARGET_HEART_RATE_LOWER_BPM_TAG,
+        defaultVal: TARGET_HEART_RATE_LOWER_BPM_DEFAULT,
+        validator: (str) {
+          final upperLimit = getStringIntegerPreference(
+            TARGET_HEART_RATE_UPPER_BPM_TAG,
+            TARGET_HEART_RATE_UPPER_BPM_DEFAULT,
+            TARGET_HEART_RATE_UPPER_BPM_DEFAULT_INT,
+          );
+          if (!isInteger(str, 0, upperLimit)) {
+            return "Invalid lower target HR (should be 0 <= size <= $upperLimit)";
+          }
+          return null;
+        },
+      ),
+      TextFieldPreference(
+        TARGET_HEART_RATE_UPPER_BPM,
+        TARGET_HEART_RATE_UPPER_BPM_TAG,
+        defaultVal: TARGET_HEART_RATE_UPPER_BPM_DEFAULT,
+        validator: (str) {
+          final lowerLimit = getStringIntegerPreference(
+            TARGET_HEART_RATE_LOWER_BPM_TAG,
+            TARGET_HEART_RATE_LOWER_BPM_DEFAULT,
+            TARGET_HEART_RATE_LOWER_BPM_DEFAULT_INT,
+          );
+          if (!isInteger(str, lowerLimit, 300)) {
+            return "Invalid heart rate limit (should be $lowerLimit <= size <= 300)";
+          }
+          return null;
+        },
+      ),
+      TextFieldPreference(
+        TARGET_HEART_RATE_LOWER_ZONE,
+        TARGET_HEART_RATE_LOWER_ZONE_TAG,
+        defaultVal: TARGET_HEART_RATE_LOWER_ZONE_DEFAULT,
+        validator: (str) {
+          final upperLimit = getStringIntegerPreference(
+            TARGET_HEART_RATE_UPPER_ZONE_TAG,
+            TARGET_HEART_RATE_UPPER_ZONE_DEFAULT,
+            TARGET_HEART_RATE_UPPER_ZONE_DEFAULT_INT,
+          );
+          if (!isInteger(str, 0, upperLimit)) {
+            return "Invalid lower zone (should be 0 <= size <= $upperLimit)";
+          }
+          return null;
+        },
+      ),
+      TextFieldPreference(
+        TARGET_HEART_RATE_UPPER_ZONE,
+        TARGET_HEART_RATE_UPPER_ZONE_TAG,
+        defaultVal: TARGET_HEART_RATE_UPPER_ZONE_DEFAULT,
+        validator: (str) {
+          final lowerLimit = getStringIntegerPreference(
+            TARGET_HEART_RATE_LOWER_ZONE_TAG,
+            TARGET_HEART_RATE_LOWER_ZONE_DEFAULT,
+            TARGET_HEART_RATE_LOWER_ZONE_DEFAULT_INT,
+          );
+          if (!isInteger(str, lowerLimit, 7)) {
+            return "Invalid upper zone (should be $lowerLimit <= size <= 300)";
+          }
+          return null;
+        },
+      ),
+      PreferenceTitle(TARGET_HEART_RATE_AUDIO_MODE_DESCRIPTION, style: descriptionStyle),
+      PreferenceDialogLink(
+        TARGET_HEART_RATE_AUDIO_MODE,
+        dialog: PreferenceDialog(
+          [
+            RadioPreference(
+              TARGET_HEART_RATE_AUDIO_MODE_NONE_DESCRIPTION,
+              TARGET_HEART_RATE_AUDIO_MODE_NONE,
+              TARGET_HEART_RATE_AUDIO_MODE_TAG,
+            ),
+            RadioPreference(
+              TARGET_HEART_RATE_AUDIO_MODE_SINGLE_DESCRIPTION,
+              TARGET_HEART_RATE_AUDIO_MODE_SINGLE,
+              TARGET_HEART_RATE_AUDIO_MODE_TAG,
+            ),
+            RadioPreference(
+              TARGET_HEART_RATE_AUDIO_MODE_PERIODIC_DESCRIPTION,
+              TARGET_HEART_RATE_AUDIO_MODE_PERIODIC,
+              TARGET_HEART_RATE_AUDIO_MODE_TAG,
+            ),
+          ],
+          title: 'Select Target HR Audio Mode',
+          cancelText: 'Close',
+        ),
+      ),
+      PreferenceTitle(TARGET_HEART_RATE_SOUND_EFFECT_DESCRIPTION, style: descriptionStyle),
+      PreferenceDialogLink(
+        TARGET_HEART_RATE_SOUND_EFFECT,
+        dialog: PreferenceDialog(
+          [
+            RadioPreference(
+              TARGET_HEART_RATE_SOUND_EFFECT_ONE_TONE_DESCRIPTION,
+              TARGET_HEART_RATE_SOUND_EFFECT_ONE_TONE,
+              TARGET_HEART_RATE_SOUND_EFFECT_TAG,
+            ),
+            RadioPreference(
+              TARGET_HEART_RATE_SOUND_EFFECT_TWO_TONE_DESCRIPTION,
+              TARGET_HEART_RATE_SOUND_EFFECT_TWO_TONE,
+              TARGET_HEART_RATE_SOUND_EFFECT_TAG,
+            ),
+            RadioPreference(
+              TARGET_HEART_RATE_SOUND_EFFECT_THREE_TONE_DESCRIPTION,
+              TARGET_HEART_RATE_SOUND_EFFECT_THREE_TONE,
+              TARGET_HEART_RATE_SOUND_EFFECT_TAG,
+            ),
+            RadioPreference(
+              TARGET_HEART_RATE_SOUND_EFFECT_BLEEP_DESCRIPTION,
+              TARGET_HEART_RATE_SOUND_EFFECT_BLEEP,
+              TARGET_HEART_RATE_SOUND_EFFECT_TAG,
+            ),
+          ],
+          title: 'Select Target HR Sound Effect',
+          cancelText: 'Close',
+        ),
+      ),
+    ];
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Target HR Prefs')),
+      body: PreferencePage(targetHrPreferences),
+    );
+  }
+}
