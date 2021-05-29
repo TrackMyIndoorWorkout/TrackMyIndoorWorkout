02d9ab3dc273b1d756ff5597f007cabf6d81d8f8
MrCsabaToth <csaba.toth.us@gmail.com>
2021-05-28 19:46:12 -0700

    #141 Preparing to have the whole leaderboard for a better PaceLight

--- lib/persistence/dao/workout_summary_dao.dart
+++ lib/persistence/dao/workout_summary_dao.dart
@@ -9,9 +9,15 @@ abstract class WorkoutSummaryDao {
   @Query('SELECT * FROM $WORKOUT_SUMMARIES_TABLE_NAME WHERE id = :id')
   Stream<WorkoutSummary> findWorkoutSummaryById(int id);
 
+  @Query('SELECT * FROM $WORKOUT_SUMMARIES_TABLE_NAME WHERE device_id = :deviceId ORDER BY speed')
+  Future<List<WorkoutSummary>> findAllWorkoutSummariesByDevice(String deviceId);
+
   @Query(
       'SELECT * FROM $WORKOUT_SUMMARIES_TABLE_NAME WHERE device_id = :deviceId ORDER BY speed DESC LIMIT :limit OFFSET :offset')
   Future<List<WorkoutSummary>> findWorkoutSummaryByDevice(String deviceId, int limit, int offset);
+
+  @Query('SELECT * FROM $WORKOUT_SUMMARIES_TABLE_NAME WHERE sport = :sport ORDER BY speed')
+  Future<List<WorkoutSummary>> findAllWorkoutSummariesBySport(String sport);
 
   @Query(
       'SELECT * FROM $WORKOUT_SUMMARIES_TABLE_NAME WHERE sport = :sport ORDER BY speed DESC LIMIT :limit OFFSET :offset')
