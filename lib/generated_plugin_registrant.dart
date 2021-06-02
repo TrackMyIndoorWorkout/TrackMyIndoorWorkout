f9c0459ddfddc494f454446623d8b493a0ba4981
MrCsabaToth <csaba.toth.us@gmail.com>
2021-06-02 16:05:36 -0700

    #118 Another round of version package bumps for sound null safety. Now Pub Get runs, but the source ahs a ton of errors + we need to migrate to pref.

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -5,9 +5,9 @@
 // ignore_for_file: lines_longer_than_80_chars
 
 import 'package:file_picker/src/file_picker_web.dart';
-import 'package:import_js_library/import_js_library.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:soundpool_web/soundpool_web.dart';
+import 'package:uni_links_web2/uni_links_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 import 'package:wakelock_web/wakelock_web.dart';
 
@@ -16,9 +16,9 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
   FilePickerWeb.registerWith(registrar);
-  ImportJsLibrary.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
   SoundpoolPlugin.registerWith(registrar);
+  UniLinksPlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
   WakelockWeb.registerWith(registrar);
   registrar.registerMessageHandler();
