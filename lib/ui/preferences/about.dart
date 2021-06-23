9a263eec44954eb9fa03642449279bd2d2bdba72
MrCsabaToth <csaba.toth.us@gmail.com>
2021-06-23 15:54:23 -0700

    #160 Foundations for About Screen

--- lib/ui/preferences/about.dart
+++ lib/ui/preferences/about.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
 import '../../utils/constants.dart';
 import '../../utils/sound.dart';
 
@@ -11,20 +13,33 @@ class AboutScreen extends StatefulWidget {
 }
 
 class AboutScreenState extends State<AboutScreen> {
-  double _sizeDefault = 10.0;
-  TextStyle _textStyle = TextStyle();
+  String _appName = "...";
+  String _packageName = "...";
+  String _version = "...";
+  String _buildNumber = "...";
+  TextStyle _fieldStyle = TextStyle();
+  TextStyle _valueStyle = TextStyle();
 
   @override
   void initState() {
     super.initState();
-    _textStyle = Get.textTheme.headline5!.apply(
+    _fieldStyle = Get.textTheme.headline5!;
+    _valueStyle = Get.textTheme.headline6!.apply(
       fontFamily: FONT_FAMILY,
-      color: Colors.white,
+      // color: Colors.white,
     );
-    _sizeDefault = _textStyle.fontSize! * 2;
     if (!Get.isRegistered<SoundService>()) {
       Get.put<SoundService>(SoundService());
     }
+
+    PackageInfo.fromPlatform().then((PackageInfo packageInfo) {
+      setState(() {
+        _appName = packageInfo.appName;
+        _packageName = packageInfo.packageName;
+        _version = packageInfo.version;
+        _buildNumber = packageInfo.buildNumber;
+      });
+    });
   }
 
   @override
@@ -37,11 +52,87 @@ class AboutScreenState extends State<AboutScreen> {
           children: [
             Flexible(
               child: Text(
-                'Bluetooth Adapter is.\n' + 'Location permission is',
-                style: _textStyle,
+                'App Name:',
+                style: _fieldStyle,
                 maxLines: 10,
                 overflow: TextOverflow.ellipsis,
                 textAlign: TextAlign.center,
+              ),
+            ),
+            Flexible(
+              child: Text(
+                _appName,
+                style: _valueStyle,
+                maxLines: 10,
+                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Flexible(
+              child: Text(
+                'Package Name:',
+                style: _fieldStyle,
+                maxLines: 10,
+                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Flexible(
+              child: Text(
+                _packageName,
+                style: _valueStyle,
+                maxLines: 10,
+                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Flexible(
+              child: Text(
+                'Version:',
+                style: _fieldStyle,
+                maxLines: 10,
+                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Flexible(
+              child: Text(
+                _version,
+                style: _valueStyle,
+                maxLines: 10,
+                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Flexible(
+              child: Text(
+                'Build#:',
+                style: _fieldStyle,
+                maxLines: 10,
+                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Flexible(
+              child: Text(
+                _buildNumber,
+                style: _valueStyle,
+                maxLines: 10,
+                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Center(
+              child: ElevatedButton.icon(
+                icon: Icon(Icons.help),
+                label: Text("Help"),
+                onPressed: () async {
+                  if (await canLaunch(HELP_URL)) {
+                    launch(HELP_URL);
+                  } else {
+                    Get.snackbar("Attention", "Cannot open URL");
+                  }
+                },
               ),
             ),
           ],
