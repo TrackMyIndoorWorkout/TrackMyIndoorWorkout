019204925ce86db28c23a5ff8e55432bd6fc235b
MrCsabaToth <csaba.toth.us@gmail.com>
2021-07-19 16:19:03 -0700

    Adding separate buttons for Quick Start, FAQ, and Known Issues URLs

--- lib/ui/about.dart
+++ lib/ui/about.dart
@@ -13,8 +13,12 @@ class AboutScreen extends StatefulWidget {
 }
 
 class AboutScreenState extends State<AboutScreen> {
+  static const HOST_URL = "https://trackmyindoorworkout.github.io/";
+  static const QUICK_START_URL = "${HOST_URL}2020/09/25/quick-start.html";
+  static const FAQ_URL = "${HOST_URL}2020/09/22/frequently-asked-questions.html";
+  static const KNOWN_ISSUES_URL = "${HOST_URL}2020/09/26/known-issues.html";
+
   late String _appName;
-  late String _packageName;
   late String _version;
   late String _buildNumber;
   TextStyle _fieldStyle = TextStyle();
@@ -30,7 +34,6 @@ class AboutScreenState extends State<AboutScreen> {
     );
     final packageInfo = Get.find<PackageInfo>();
     _appName = packageInfo.appName;
-    _packageName = packageInfo.packageName;
     _version = packageInfo.version;
     _buildNumber = packageInfo.buildNumber;
 
@@ -41,7 +44,6 @@ class AboutScreenState extends State<AboutScreen> {
     PackageInfo.fromPlatform().then((PackageInfo packageInfo) {
       setState(() {
         _appName = packageInfo.appName;
-        _packageName = packageInfo.packageName;
         _version = packageInfo.version;
         _buildNumber = packageInfo.buildNumber;
       });
@@ -68,24 +70,6 @@ class AboutScreenState extends State<AboutScreen> {
             Flexible(
               child: Text(
                 _appName,
-                style: _valueStyle,
-                maxLines: 10,
-                overflow: TextOverflow.ellipsis,
-                textAlign: TextAlign.center,
-              ),
-            ),
-            Flexible(
-              child: Text(
-                'Package Name:',
-                style: _fieldStyle,
-                maxLines: 10,
-                overflow: TextOverflow.ellipsis,
-                textAlign: TextAlign.center,
-              ),
-            ),
-            Flexible(
-              child: Text(
-                _packageName,
                 style: _valueStyle,
                 maxLines: 10,
                 overflow: TextOverflow.ellipsis,
@@ -128,13 +112,40 @@ class AboutScreenState extends State<AboutScreen> {
                 textAlign: TextAlign.center,
               ),
             ),
+            Divider(),
             Center(
               child: ElevatedButton.icon(
-                icon: Icon(Icons.help),
-                label: Text("Help"),
+                icon: Icon(Icons.open_in_new),
+                label: Text("Quick Start"),
                 onPressed: () async {
-                  if (await canLaunch(HELP_URL)) {
-                    launch(HELP_URL);
+                  if (await canLaunch(QUICK_START_URL)) {
+                    launch(QUICK_START_URL);
+                  } else {
+                    Get.snackbar("Attention", "Cannot open URL");
+                  }
+                },
+              ),
+            ),
+            Center(
+              child: ElevatedButton.icon(
+                icon: Icon(Icons.open_in_new),
+                label: Text("Frequently Asked Questions"),
+                onPressed: () async {
+                  if (await canLaunch(FAQ_URL)) {
+                    launch(FAQ_URL);
+                  } else {
+                    Get.snackbar("Attention", "Cannot open URL");
+                  }
+                },
+              ),
+            ),
+            Center(
+              child: ElevatedButton.icon(
+                icon: Icon(Icons.open_in_new),
+                label: Text("Known Issues"),
+                onPressed: () async {
+                  if (await canLaunch(KNOWN_ISSUES_URL)) {
+                    launch(KNOWN_ISSUES_URL);
                   } else {
                     Get.snackbar("Attention", "Cannot open URL");
                   }
