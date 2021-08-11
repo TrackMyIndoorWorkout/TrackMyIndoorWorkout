2611fb9bdb25f03476688275422f775a94578386
MrCsabaToth <csaba.toth.us@gmail.com>
2021-08-11 12:52:22 -0700

    Making filter parameter typed (String) #189

--- lib/devices/bluetooth_device_ex.dart
+++ lib/devices/bluetooth_device_ex.dart
@@ -3,12 +3,12 @@ import 'package:flutter_blue/flutter_blue.dart';
 import '../utils/guid_ex.dart';
 
 extension BluetoothDeviceEx on BluetoothDevice {
-  static BluetoothService? filterService(List<BluetoothService> services, identifier) {
+  static BluetoothService? filterService(List<BluetoothService> services, String identifier) {
     return services.firstWhereOrNull((service) => service.uuid.uuidString() == identifier);
   }
 
   static BluetoothCharacteristic? filterCharacteristic(
-      List<BluetoothCharacteristic>? characteristics, identifier) {
+      List<BluetoothCharacteristic>? characteristics, String identifier) {
     return characteristics?.firstWhereOrNull((ch) => ch.uuid.uuidString() == identifier);
   }
 }
