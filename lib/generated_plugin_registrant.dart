77bf8185e1e0b7864f3e5d263592592501f24e07
MrCsabaToth <csaba.toth.us@gmail.com>
2021-09-11 21:38:14 -0700

    Regenerated plugin registrant code. I manually moved the BluetoothEnablePlugin and ShareFilesAndScreenshotWidgetsPlugin to the end so V1 API plugins would follow V2 API plugins

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -2,9 +2,10 @@
 // Generated file. Do not edit.
 //
 
+// ignore_for_file: directives_ordering
 // ignore_for_file: lines_longer_than_80_chars
 
-import 'package:file_picker/src/file_picker_web.dart';
+import 'package:file_picker/_internal/file_picker_web.dart';
 import 'package:flutter_native_timezone/flutter_native_timezone_web.dart';
 import 'package:package_info_plus_web/package_info_plus_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
