79ebb0e4e2c8bf20d969b11f0faf09bccfc3aaa6
MrCsabaToth <csaba.toth.us@gmail.com>
2021-09-18 22:43:29 -0700

    UA linting (by IDE) code changes #194

--- lib/upload/under_armour/under_armour_service.dart
+++ lib/upload/under_armour/under_armour_service.dart
@@ -6,22 +6,26 @@ import '../upload_service.dart';
 import 'under_armour.dart';
 
 class UnderArmourService implements UploadService {
-  UnderArmour _underArmour = UnderArmour(UNDER_ARMOUR_KEY, UNDER_ARMOUR_SECRET);
+  final UnderArmour _underArmour = UnderArmour(UNDER_ARMOUR_KEY, UNDER_ARMOUR_SECRET);
 
+  @override
   Future<bool> login() async {
     return await _underArmour.oauth(_underArmour.clientId, _underArmour.secret);
   }
 
+  @override
   Future<bool> hasValidToken() async {
     return await _underArmour.hasValidToken();
   }
 
+  @override
   Future<int> deAuthorize() async {
     return await _underArmour.deAuthorize(_underArmour.clientId);
   }
 
+  @override
   Future<int> upload(Activity activity, List<Record> records) async {
-    if (records.length <= 0) {
+    if (records.isEmpty) {
       return 404;
     }
 
