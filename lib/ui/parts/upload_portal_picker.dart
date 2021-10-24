4f6b8236128e2ab155bf1ed888db7e868c931367
MrCsabaToth <csaba.toth.us@gmail.com>
2021-10-24 14:38:14 -0700

    Restyle portal picker #194

--- lib/ui/parts/upload_portal_picker.dart
+++ lib/ui/parts/upload_portal_picker.dart
@@ -11,8 +11,9 @@ class PortalChoiceDescriptor {
   final String name;
   final String assetName;
   final Color color;
+  final double heightMultiplier;
 
-  PortalChoiceDescriptor(this.name, this.assetName, this.color);
+  PortalChoiceDescriptor(this.name, this.assetName, this.color, this.heightMultiplier);
 }
 
 class UploadPortalPickerBottomSheet extends StatefulWidget {
@@ -33,23 +34,22 @@ class UploadPortalPickerBottomSheetState extends State<UploadPortalPickerBottomS
   final ThemeManager _themeManager = Get.find<ThemeManager>();
   List<PortalChoiceDescriptor> _portalChoices = [];
   TextStyle _largerTextStyle = const TextStyle();
-  TextStyle _selectedTextStyle = const TextStyle();
 
   @override
   void initState() {
     super.initState();
     _portalChoices = [
-      PortalChoiceDescriptor(_portalNames[0], "assets/strava.svg", _themeManager.getOrangeColor()),
       PortalChoiceDescriptor(
-          _portalNames[1], "assets/suunto.svg", _themeManager.getSuuntoRedColor()),
+          _portalNames[0], "assets/strava.svg", _themeManager.getOrangeColor(), 1.5),
       PortalChoiceDescriptor(
-          _portalNames[2], "assets/under-armour.svg", _themeManager.getSuuntoRedColor()),
+          _portalNames[1], "assets/suunto.svg", _themeManager.getSuuntoRedColor(), 2.0),
       PortalChoiceDescriptor(
-          _portalNames[3], "assets/under-armour.svg", _themeManager.getBlueColor()),
+          _portalNames[2], "assets/under-armour-2line.svg", _themeManager.getSuuntoRedColor(), 2.0),
+      PortalChoiceDescriptor(
+          _portalNames[3], "assets/training-peaks-2line.svg", _themeManager.getBlueColor(), 2.0),
     ];
     _portalIndex = max(0, _portalNames.indexOf("Strava"));
     _largerTextStyle = Get.textTheme.headline4!;
-    _selectedTextStyle = _largerTextStyle.apply(color: _themeManager.getProtagonistColor());
   }
 
   @override
@@ -57,46 +57,42 @@ class UploadPortalPickerBottomSheetState extends State<UploadPortalPickerBottomS
     List<Widget> choiceRows = [];
     choiceRows.addAll(
       _portalChoices.asMap().entries.map(
-            (e) => Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
+            (e) => Column(
               children: [
-                Transform.scale(
-                  scale: 2,
-                  child: Radio(
-                    value: e.key,
-                    groupValue: _portalIndex,
-                    onChanged: (value) {
-                      setState(() {
-                        _portalIndex = value as int;
-                      });
-                    },
-                  ),
-                ),
-                e.value.assetName.isNotEmpty
-                    ? GestureDetector(
-                        onTap: () {
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Transform.scale(
+                      scale: 2,
+                      child: Radio(
+                        value: e.key,
+                        groupValue: _portalIndex,
+                        onChanged: (value) {
                           setState(() {
-                            _portalIndex = e.key;
+                            _portalIndex = value as int;
                           });
                         },
-                        child: SvgPicture.asset(
-                          e.value.assetName,
-                          color: e.value.color,
-                          height: _largerTextStyle.fontSize,
-                          semanticsLabel: '${e.value.name} Logo',
-                        ),
-                      )
-                    : Container(),
-                TextButton(
-                  onPressed: () {
-                    setState(() {
-                      _portalIndex = e.key;
-                    });
-                  },
-                  child: Text("/${e.value.name}",
-                      style: _portalIndex == e.key ? _selectedTextStyle : _largerTextStyle),
+                      ),
+                    ),
+                    e.value.assetName.isNotEmpty
+                        ? GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                _portalIndex = e.key;
+                              });
+                            },
+                            child: SvgPicture.asset(
+                              e.value.assetName,
+                              color: e.value.color,
+                              height: _largerTextStyle.fontSize! * e.value.heightMultiplier,
+                              semanticsLabel: '${e.value.name} Logo',
+                            ),
+                          )
+                        : Container(),
+                  ],
                 ),
+                Divider(height: _largerTextStyle.fontSize! / 2),
               ],
             ),
           ),
