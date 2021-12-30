716853866daaab0ac4a9bd6d1c48bca5e3b23a20
MrCsabaToth <csaba.toth.us@gmail.com>
2021-12-30 15:05:25 -0800

    Test for Matrix Treadmill, WIP, thanks @corelema for data #223

---
+++ test/matrix_treadmill_test.dart
@@ -0,0 +1,90 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:track_my_indoor_exercise/devices/device_descriptors/treadmill_device_descriptor.dart';
+import 'package:track_my_indoor_exercise/devices/device_map.dart';
+import 'package:track_my_indoor_exercise/persistence/models/record.dart';
+import 'package:track_my_indoor_exercise/utils/constants.dart';
+
+class TestPair {
+  final List<int> data;
+  final RecordWithSport record;
+
+  const TestPair({required this.data, required this.record});
+}
+
+void main() {
+  test('Treadmill constructor tests', () async {
+    final treadmill = deviceMap["GRun"]!;
+
+    expect(treadmill.canMeasureHeartRate, false);
+    expect(treadmill.defaultSport, ActivityType.run);
+    expect(treadmill.fourCC, "GRun");
+  });
+
+  test('GRun interprets FTMS Matrix Data flags properly', () async {
+    final treadmill = deviceMap["GRun"] as TreadmillDeviceDescriptor;
+    const lsb = 158;
+    const msb = 27;
+    const flag = maxUint8 * msb + lsb;
+    treadmill.stopWorkout();
+
+    treadmill.processFlag(flag);
+
+    expect(treadmill.speedMetric, isNotNull);
+    expect(treadmill.distanceMetric, isNotNull);
+    expect(treadmill.paceMetric, null);
+    expect(treadmill.powerMetric, isNotNull);
+    expect(treadmill.caloriesMetric, isNotNull);
+    expect(treadmill.caloriesPerHourMetric, isNotNull);
+    expect(treadmill.caloriesPerMinuteMetric, isNotNull);
+    expect(treadmill.heartRateByteIndex, 22);
+    expect(treadmill.timeMetric, null);
+    expect(treadmill.powerMetric, isNotNull);
+  });
+
+  group('GRun interprets FTMS Matrix Data properly', () {
+    for (final testPair in [
+      TestPair(
+        data: [158, 27, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 127, 0, 0, 255, 255, 0, 0, 0],
+        record: RecordWithSport(
+          distance: null,
+          elapsed: null,
+          calories: null,
+          power: 122,
+          speed: 29.1,
+          cadence: 120,
+          heartRate: 84,
+          pace: null,
+          sport: ActivityType.run,
+          caloriesPerHour: null,
+          caloriesPerMinute: null,
+        ),
+      ),
+    ]) {
+      final sum = testPair.data.fold<double>(0.0, (a, b) => a + b);
+      test("$sum", () async {
+        final treadmill = deviceMap["GRun"]!;
+        treadmill.stopWorkout();
+
+        final record = treadmill.stubRecord(testPair.data)!;
+
+        expect(record.id, null);
+        expect(record.id, testPair.record.id);
+        expect(record.activityId, null);
+        expect(record.activityId, testPair.record.activityId);
+        expect(record.distance, testPair.record.distance);
+        expect(record.elapsed, testPair.record.elapsed);
+        expect(record.calories, testPair.record.calories);
+        expect(record.power, testPair.record.power);
+        expect(record.speed, testPair.record.speed);
+        expect(record.cadence, testPair.record.cadence);
+        expect(record.heartRate, testPair.record.heartRate);
+        expect(record.elapsedMillis, testPair.record.elapsedMillis);
+        expect(record.pace, testPair.record.pace);
+        expect(record.strokeCount, testPair.record.strokeCount);
+        expect(record.sport, testPair.record.sport);
+        expect(record.caloriesPerHour, testPair.record.caloriesPerHour);
+        expect(record.caloriesPerMinute, testPair.record.caloriesPerMinute);
+      });
+    }
+  });
+}
