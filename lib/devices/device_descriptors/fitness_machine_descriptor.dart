e9ab5baa1a44d6eeb7f09ccb3aab1f9f18393d8d
MrCsabaToth <csaba.toth.us@gmail.com>
2022-01-20 16:24:02 -0800

    FitnessMachine canDataProcessed logic change! #223

--- lib/devices/device_descriptors/fitness_machine_descriptor.dart
+++ lib/devices/device_descriptors/fitness_machine_descriptor.dart
@@ -41,11 +41,11 @@ abstract class FitnessMachineDescriptor extends DeviceDescriptor {
   @override
   bool canDataProcessed(List<int> data) {
     final dataLength = data.length;
-    if (byteCounter <= 0) {
+    if (byteCounter <= flagByteSize) {
       preProcessFlag(data);
     }
 
-    return byteCounter > 2 ? dataLength == byteCounter : dataLength > 2;
+    return byteCounter >= flagByteSize && dataLength == byteCounter;
   }
 
   int advanceFlag(int flag) {
