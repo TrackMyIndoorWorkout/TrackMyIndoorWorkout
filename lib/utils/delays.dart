d1f10438c7a65e28d3a522724bb22c63b7057473
MrCsabaToth <csaba.toth.us@gmail.com>
2022-03-17 22:30:38 -0700

    Decreasing the data throttle delays from the rounded ones to make time for processing and hopefully make the elapsed time less janky

--- lib/utils/delays.dart
+++ lib/utils/delays.dart
@@ -1,7 +1,7 @@
 // Rate-limiting delays and other thresholds in milliseconds
 const startupIntermittentDelay = 100;
-const ftmsDataThreshold = 500;
-const sensorDataThreshold = 1000;
+const ftmsDataThreshold = 450;
+const sensorDataThreshold = 950;
 const uiIntermittentDelay = 100;
 const spinDownThreshold = 500;
 const ftmsStatusThreshold = 250;
