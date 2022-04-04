4bd6a2ee5c6ded23e5c59cd1274e480533c38067
MrCsabaToth <csaba.toth.us@gmail.com>
2022-04-03 22:52:36 -0700

    Zone Color picker UI feedback enhancements / fixes #216

--- lib/ui/parts/color_picker.dart
+++ lib/ui/parts/color_picker.dart
@@ -18,13 +18,20 @@ class ColorPickerBottomSheetState extends State<ColorPickerBottomSheet> {
   final ThemeManager _themeManager = Get.find<ThemeManager>();
   late final CircleColorPickerController _controller;
   Color _color = Colors.white;
+  Color _initialColor = Colors.white;
+  TextStyle _textStyle = const TextStyle();
   double? _mediaWidth;
 
   @override
   void initState() {
     super.initState();
     _controller = CircleColorPickerController(initialColor: widget.color);
-    _color = widget.color;
+    _color = Color(widget.color.value);
+    _initialColor = Color(widget.color.value);
+    _textStyle = Get.textTheme.headline5!.apply(
+      fontFamily: fontFamily,
+      color: _themeManager.getProtagonistColor(),
+    );
   }
 
   @override
@@ -39,16 +46,33 @@ class ColorPickerBottomSheetState extends State<ColorPickerBottomSheet> {
         child: CircleColorPicker(
           controller: _controller,
           onChanged: (color) {
-            _color = color;
+            setState(() {
+              _color = color;
+            });
           },
           size: Size(_mediaWidth!, _mediaWidth!),
           strokeWidth: 5,
-          thumbSize: _mediaWidth! / 6,
+          thumbSize: _mediaWidth! / 5,
+          textStyle: _textStyle,
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
-      floatingActionButton: _themeManager.getGreenFab(
-          Icons.check, false, false, "", 0, () => Get.back(result: _color)),
+      floatingActionButton: Container(
+        margin: const EdgeInsets.all(10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _themeManager.getBlueFab(Icons.clear, false, false, "Cancel", 0, () => Get.back()),
+            const SizedBox(width: 10, height: 10),
+            _themeManager.getBlueFab(
+                Icons.refresh, false, false, "Reset", 0, () => _controller.color = _initialColor),
+            const SizedBox(width: 10, height: 10),
+            _themeManager.getGreenFab(
+                Icons.check, false, false, "Apply", 0, () => Get.back(result: _color)),
+          ],
+        ),
+      ),
     );
   }
 }
