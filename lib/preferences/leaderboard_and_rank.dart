43900609bf70dbeeed1b0ce3e3e41062e87a80f5
MrCsabaToth <csaba.toth.us@gmail.com>
2022-04-15 11:21:15 -0700

    Change my mind about average speed default: should be on #255

--- lib/preferences/leaderboard_and_rank.dart
+++ lib/preferences/leaderboard_and_rank.dart
@@ -42,7 +42,7 @@ const rankInfoOnTrackDescription =
 
 const avgSpeedOnTrack = "Display average speed at the center of the track";
 const avgSpeedOnTrackTag = "avg_speed_on_track";
-const avgSpeedOnTrackDefault = false;
+const avgSpeedOnTrackDefault = true;
 const avgSpeedOnTrackDescription =
     "On: Average speeds will be displayed for the current effort and the other"
     "leaderboard positions listed.";
