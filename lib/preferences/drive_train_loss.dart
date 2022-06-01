735c3b2591b068cb534e23f4c1664a345ff4d0e8
MrCsabaToth <csaba.toth.us@gmail.com>
2022-05-31 23:36:00 -0700

    Increase default drive train loss from 1% to 3% #240

--- lib/preferences/drive_train_loss.dart
+++ lib/preferences/drive_train_loss.dart
@@ -1,6 +1,6 @@
 const driveTrainLoss = "Drive Train Loss (%)";
 const driveTrainLossTag = "drive_train_loss";
 const driveTrainLossMin = 0;
-const driveTrainLossDefault = 1;
+const driveTrainLossDefault = 3;
 const driveTrainLossMax = 20;
 const driveTrainLossDescription = "Used for power -> speed calculations";
