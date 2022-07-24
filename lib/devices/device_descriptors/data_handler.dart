7c1fe9276e92731bc8f1f30d69718d4f7761e7f4
MrCsabaToth <csaba.toth.us@gmail.com>
2022-07-24 16:45:11 -0700

    No HR reading should return null instead of 0 #305

--- lib/devices/device_descriptors/data_handler.dart
+++ lib/devices/device_descriptors/data_handler.dart
@@ -103,7 +103,7 @@ abstract class DataHandler {
   }
 
   double? getHeartRate(List<int> data) {
-    if (heartRateByteIndex == null) return 0;
+    if (heartRateByteIndex == null || heartRateByteIndex! >= data.length) return null;
 
     return data[heartRateByteIndex!].toDouble();
   }
