f72da958851ed31be35d158c976a35137e5ab327
MrCsabaToth <csaba.toth.us@gmail.com>
2022-10-02 01:10:04 -0700

    Power meter tests #351

---
+++ test/power_meter_sensor_test.dart
@@ -0,0 +1,86 @@
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:track_my_indoor_exercise/devices/gadgets/cycling_power_sensor.dart';
+import 'package:track_my_indoor_exercise/persistence/models/record.dart';
+import 'package:track_my_indoor_exercise/utils/constants.dart';
+import 'package:track_my_indoor_exercise/utils/init_preferences.dart';
+import 'power_meter_sensor_test.mocks.dart';
+
+class TestPair {
+  final List<int> data;
+  final RecordWithSport record;
+
+  const TestPair({required this.data, required this.record});
+}
+
+const sampleData = [44, 0, 103, 0, 209, 181, 228, 0, 72, 85];
+
+@GenerateNiceMocks([MockSpec<BluetoothDevice>()])
+void main() {
+  setUpAll(() async {
+    await initPrefServiceForTest();
+  });
+
+  test('Power meter interprets flags properly', () async {
+    final powerMeter = CyclingPowerSensor(MockBluetoothDevice());
+
+    final canProcess = powerMeter.canMeasurementProcessed(sampleData);
+
+    expect(canProcess, true);
+    expect(powerMeter.powerMetric, isNotNull);
+    expect(powerMeter.wheelRevolutionMetric, null);
+    expect(powerMeter.crankRevolutionMetric, isNotNull);
+    expect(powerMeter.caloriesMetric, null);
+  });
+
+  group('Power meter Device interprets data properly', () {
+    for (final testPair in [
+      TestPair(
+        data: sampleData,
+        record: RecordWithSport(
+          distance: null,
+          elapsed: null,
+          calories: null,
+          power: 103,
+          speed: null,
+          cadence: 0,
+          heartRate: null,
+          pace: null,
+          sport: ActivityType.ride,
+          caloriesPerHour: null,
+          caloriesPerMinute: null,
+        ),
+      ),
+    ]) {
+      final sum = testPair.data.fold<double>(0.0, (a, b) => a + b);
+      test("$sum", () async {
+        final powerMeter = CyclingPowerSensor(MockBluetoothDevice());
+
+        final record = powerMeter.processMeasurement(testPair.data);
+
+        expect(record.id, null);
+        expect(record.id, testPair.record.id);
+        expect(record.activityId, null);
+        expect(record.activityId, testPair.record.activityId);
+        expect(record.distance, testPair.record.distance);
+        expect(record.elapsed, testPair.record.elapsed);
+        expect(record.calories, testPair.record.calories);
+        expect(record.power, testPair.record.power);
+        if (testPair.record.speed != null) {
+          expect(record.speed, closeTo(testPair.record.speed!, eps));
+        } else {
+          expect(record.speed, null);
+        }
+        expect(record.cadence, testPair.record.cadence);
+        expect(record.heartRate, testPair.record.heartRate);
+        expect(record.elapsedMillis, testPair.record.elapsedMillis);
+        expect(record.pace, testPair.record.pace);
+        expect(record.strokeCount, testPair.record.strokeCount);
+        expect(record.sport, testPair.record.sport);
+        expect(record.caloriesPerHour, testPair.record.caloriesPerHour);
+        expect(record.caloriesPerMinute, testPair.record.caloriesPerMinute);
+      });
+    }
+  });
+}
