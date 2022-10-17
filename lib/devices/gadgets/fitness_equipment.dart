c250ed5c482b28dd73944b91295d077911d2dd1a
MrCsabaToth <csaba.toth.us@gmail.com>
2022-10-16 22:36:55 -0700

    Try to avoid unwanted power calculation from speed. Not sure if it'll fix #359, but this definitely should only be applied if there's no native power reporting #358

--- lib/devices/gadgets/fitness_equipment.dart
+++ lib/devices/gadgets/fitness_equipment.dart
@@ -991,7 +991,9 @@ class FitnessEquipment extends DeviceBase with PowerSpeedMixin {
           stub.power = (stub.caloriesPerMinute! * 50.0 / 3.0).round(); // 60 * 1000 / 3600
         } else if ((stub.caloriesPerHour ?? 0.0) > eps) {
           stub.power = (stub.caloriesPerHour! * 5.0 / 18.0).round(); // 1000 / 3600
-        } else if (sport == ActivityType.ride && (stub.speed ?? 0) > displayEps) {
+        } else if (!hasPowerReporting &&
+            sport == ActivityType.ride &&
+            (stub.speed ?? 0) > displayEps) {
           // When cycling supplement power from speed if missing
           // via https://www.gribble.org/cycling/power_v_speed.html
           stub.power = powerForVelocity(stub.speed! * DeviceDescriptor.kmh2ms).toInt();
@@ -1016,8 +1018,9 @@ class FitnessEquipment extends DeviceBase with PowerSpeedMixin {
       }
     }
 
-    if ((stub.power ?? 0) < eps &&
+    if (!hasPowerReporting &&
         sport == ActivityType.ride &&
+        (stub.power ?? 0) < eps &&
         stub.speed != null &&
         stub.speed! > displayEps) {
       // When cycling supplement power from speed if missing
