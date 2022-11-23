2d896864e0697458ab6706a438d9baf97906d4ad
MrCsabaToth <csaba.toth.us@gmail.com>
2022-11-23 00:48:47 -0800

    Adding preferences option for wheel circumference #379

--- lib/ui/preferences/equipment.dart
+++ lib/ui/preferences/equipment.dart
@@ -5,13 +5,29 @@ import '../../preferences/bike_weight.dart';
 import '../../preferences/block_signal_start_stop.dart';
 import '../../preferences/drag_force_tune.dart';
 import '../../preferences/drive_train_loss.dart';
+import '../../preferences/wheel_circumference.dart';
 import 'preferences_screen_mixin.dart';
 
-class EquipmentPreferencesScreen extends StatelessWidget with PreferencesScreenMixin {
+class EquipmentPreferencesScreen extends StatefulWidget with PreferencesScreenMixin {
   static String shortTitle = "Equipment";
   static String title = "$shortTitle Preferences";
 
   const EquipmentPreferencesScreen({Key? key}) : super(key: key);
+
+  @override
+  EquipmentPreferencesScreenState createState() => EquipmentPreferencesScreenState();
+}
+
+class EquipmentPreferencesScreenState extends State<EquipmentPreferencesScreen> {
+  int _wheelCircumferenceEdit = 0;
+
+  void onWheelCircumferenceSpinTap(int delta) {
+    setState(() {
+      final circumference = PrefService.of(context).get(wheelCircumferenceTag);
+      PrefService.of(context).set(wheelCircumferenceTag, circumference + delta);
+      _wheelCircumferenceEdit++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,6 +41,33 @@ class EquipmentPreferencesScreen extends StatelessWidget with PreferencesScreenM
         max: bikeWeightMax,
         divisions: bikeWeightDivisions,
         direction: Axis.vertical,
+      ),
+      PrefSlider<int>(
+        key: Key("wheelCircumference$_wheelCircumferenceEdit"),
+        title: const Text(wheelCircumference),
+        subtitle: const Text(wheelCircumferenceDescription),
+        pref: wheelCircumferenceTag,
+        trailing: (num value) => Text("$value mm"),
+        min: wheelCircumferenceMin,
+        max: wheelCircumferenceMax,
+        divisions: wheelCircumferenceDivisions,
+        direction: Axis.vertical,
+      ),
+      PrefButton(
+        onTap: () => onWheelCircumferenceSpinTap(1),
+        child: const Text("+1 mm"),
+      ),
+      PrefButton(
+        onTap: () => onWheelCircumferenceSpinTap(-1),
+        child: const Text("-1 mm"),
+      ),
+      PrefButton(
+        onTap: () => onWheelCircumferenceSpinTap(10),
+        child: const Text("+10 mm"),
+      ),
+      PrefButton(
+        onTap: () => onWheelCircumferenceSpinTap(-10),
+        child: const Text("-10 mm"),
       ),
       PrefSlider<int>(
         title: const Text(driveTrainLoss),
@@ -64,7 +107,7 @@ class EquipmentPreferencesScreen extends StatelessWidget with PreferencesScreenM
     ];
 
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
+      appBar: AppBar(title: Text(EquipmentPreferencesScreen.title)),
       body: PrefPage(children: equipmentPreferences),
     );
   }
