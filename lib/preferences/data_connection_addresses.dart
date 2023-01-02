5e2efdae1843dac9a062a6d29d0e583d89dc3f0b
MrCsabaToth <csaba.toth.us@gmail.com>
2023-01-02 00:25:17 -0800

    Data connection addresses settings description correction: port is separated by comma -> colon #390

--- lib/preferences/data_connection_addresses.dart
+++ lib/preferences/data_connection_addresses.dart
@@ -5,7 +5,7 @@ const dataConnectionAddressesOldDefault =
     "52.44.84.95,54.160.234.139,52.87.57.116,3.93.102.29,54.157.131.119,3.226.9.14";
 
 const dataConnectionAddressesDescription =
-    "Following is a comma separated list of IP addresses with optional comma "
+    "Following is a comma separated list of IP addresses with optional colon "
     "separated port numbers. Lack of a port number will mean 443 (HTTPS). "
     "The application will reach out to these endpoints to determine if "
     "there is really a data connection.";
