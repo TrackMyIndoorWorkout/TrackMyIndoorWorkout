8d8398100383d36eca7e490db976b46bc7a32a4a
MrCsabaToth <csaba.toth.us@gmail.com>
2023-02-10 13:42:16 -0800

    Fixing FIT ID test after latest Virtufit change #404 - giving an exception because XEBEX != Wahoo Fitness, LLC

--- test/get_fit_manufacturer_test.dart
+++ test/get_fit_manufacturer_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:track_my_indoor_exercise/devices/device_factory.dart';
+import 'package:track_my_indoor_exercise/devices/device_fourcc.dart';
 import 'package:track_my_indoor_exercise/export/fit/fit_manufacturer.dart';
 
 class TestData {
@@ -15,8 +16,10 @@ void main() {
       test(
           "${deviceDescriptor.fourCC} (${deviceDescriptor.manufacturerNamePart}) -> ${deviceDescriptor.manufacturerFitId}",
           () async {
-        expect(getFitManufacturer(deviceDescriptor.manufacturerNamePart),
-            deviceDescriptor.manufacturerFitId);
+        final manufacturerFitId = deviceDescriptor.fourCC != virtufitUltimatePro2FourCC
+            ? getFitManufacturer(deviceDescriptor.manufacturerNamePart)
+            : wahooFitnessFitId;
+        expect(manufacturerFitId, deviceDescriptor.manufacturerFitId);
       });
     });
   });
