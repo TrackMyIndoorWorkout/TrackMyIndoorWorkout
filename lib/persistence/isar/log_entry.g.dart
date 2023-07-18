c85458fee8b7286685fdf8dbd4feccfbe67c29fc
MrCsabaToth <csaba.toth.us@gmail.com>
2023-07-18 15:44:32 -0700

    Regen files after Isar version upgrade #269

--- lib/persistence/isar/log_entry.g.dart
+++ lib/persistence/isar/log_entry.g.dart
@@ -7,7 +7,7 @@ part of 'log_entry.dart';
 // **************************************************************************
 
 // coverage:ignore-file
-// ignore_for_file: duplicate_ignore, non_constant_identifier_names, constant_identifier_names, invalid_use_of_protected_member, unnecessary_cast, prefer_const_constructors, lines_longer_than_80_chars, require_trailing_commas, inference_failure_on_function_invocation, unnecessary_parenthesis, unnecessary_raw_strings, unnecessary_null_checks, join_return_with_assignment, prefer_final_locals, avoid_js_rounded_ints, avoid_positional_boolean_parameters
+// ignore_for_file: duplicate_ignore, non_constant_identifier_names, constant_identifier_names, invalid_use_of_protected_member, unnecessary_cast, prefer_const_constructors, lines_longer_than_80_chars, require_trailing_commas, inference_failure_on_function_invocation, unnecessary_parenthesis, unnecessary_raw_strings, unnecessary_null_checks, join_return_with_assignment, prefer_final_locals, avoid_js_rounded_ints, avoid_positional_boolean_parameters, always_specify_types
 
 extension GetLogEntryCollection on Isar {
   IsarCollection<LogEntry> get logEntrys => this.collection();
@@ -54,7 +54,7 @@ const LogEntrySchema = CollectionSchema(
   getId: _logEntryGetId,
   getLinks: _logEntryGetLinks,
   attach: _logEntryAttach,
-  version: '3.0.5',
+  version: '3.1.0+1',
 );
 
 int _logEntryEstimateSize(
