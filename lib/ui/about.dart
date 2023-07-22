adddea00c86739956f229a6806a7a614ccd09283
MrCsabaToth <csaba.toth.us@gmail.com>
2023-07-21 23:39:18 -0700

    Simplify Isar database clear #269

--- lib/ui/about.dart
+++ lib/ui/about.dart
@@ -7,14 +7,6 @@ import 'package:package_info_plus/package_info_plus.dart';
 import 'package:pref/pref.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
-import '../../persistence/isar/activity.dart';
-import '../../persistence/isar/calorie_tune.dart';
-import '../../persistence/isar/device_usage.dart';
-import '../../persistence/isar/power_tune.dart';
-import '../../persistence/isar/workout_summary.dart';
-import '../../persistence/isar/record.dart';
-import '../../persistence/isar/floor_migration.dart';
-import '../../persistence/isar/floor_record_migration.dart';
 import '../preferences/database_migration_needed.dart';
 import '../preferences/enforced_time_zone.dart';
 // import '../preferences/welcome_presented.dart';
@@ -74,16 +66,7 @@ class AboutScreenState extends State<AboutScreen> {
           Get.find<BasePrefService>()
               .set(databaseMigrationNeededTag, databaseMigrationNeededDefault);
           final database = Get.find<Isar>();
-          database.writeTxnSync(() {
-            database.floorRecordMigrations.clearSync();
-            database.floorMigrations.clearSync();
-            database.records.clearSync();
-            database.workoutSummarys.clearSync();
-            database.powerTunes.clearSync();
-            database.deviceUsages.clearSync();
-            database.calorieTunes.clearSync();
-            database.activitys.clearSync();
-          });
+          database.clearSync();
           // Get.find<BasePrefService>().set(welcomePresentedTag, welcomePresentedDefault);
         },
       ));
