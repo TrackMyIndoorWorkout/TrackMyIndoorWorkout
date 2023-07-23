44c49ac172c73eabbdbd58f4b6549d94007ec7a3
MrCsabaToth <csaba.toth.us@gmail.com>
2023-07-22 23:20:37 -0700

    Sooth analyzer `deprecated_export_use`, see https://github.com/dart-lang/sdk/issues/51890

--- lib/ui/preferences/expert.dart
+++ lib/ui/preferences/expert.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
+import 'dart:typed_data';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
