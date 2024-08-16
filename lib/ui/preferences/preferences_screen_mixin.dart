c6a18556a56a33cbdad90aef0dd7ff6da423bd3c
MrCsabaToth <csaba.toth.us@gmail.com>
2024-08-16 12:18:27 -0700

    Code style change advised by VGV analyzer

--- lib/ui/preferences/preferences_screen_mixin.dart
+++ lib/ui/preferences/preferences_screen_mixin.dart
@@ -3,7 +3,7 @@ mixin PreferencesScreenMixin {
   static String title = shortTitle;
 
   bool isNumber(String str, double lowerLimit, double upperLimit) {
-    double? number = double.tryParse(str);
+    final number = double.tryParse(str);
     return number != null &&
         (lowerLimit < 0.0 || number >= lowerLimit) &&
         (upperLimit < 0.0 || number <= upperLimit);
