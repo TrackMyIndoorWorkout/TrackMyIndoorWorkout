c3bd42a07f76ce6ed1f9075f84a05ce14cf56538
MrCsabaToth <csaba.toth.us@gmail.com>
2025-01-01 10:32:58 -0800

    onStageStatisticsTypeDefault none -> alternating, periodDefault 3s -> 1s #539

--- lib/preferences/stage_mode.dart
+++ lib/preferences/stage_mode.dart
@@ -15,7 +15,7 @@ const onStageStatisticsType = "On-Stage Statistics Type";
 const onStageStatisticsTypeTag = "on_stage_statistics_type";
 const onStageStatisticsTypeDescription = "Select the on-stage statistics type "
     "for non cumulative measurement metrics.";
-const onStageStatisticsTypeDefault = onStageStatisticsTypeNone;
+const onStageStatisticsTypeDefault = onStageStatisticsTypeAlternating;
 const onStageStatisticsTypeNone = "none";
 const onStageStatisticsTypeNoneTitle = "None";
 const onStageStatisticsTypeNoneDescription = "Don't display statistics";
@@ -33,7 +33,7 @@ const onStageStatisticsTypeAlternatingDescription =
 const onStageStatisticsAlternationPeriod = "Stat Alternation Period (s)";
 const onStageStatisticsAlternationPeriodTag = "on_stage_statistics_alternation_period";
 const onStageStatisticsAlternationPeriodMin = 1;
-const onStageStatisticsAlternationPeriodDefault = 3;
+const onStageStatisticsAlternationPeriodDefault = onStageStatisticsAlternationPeriodMin;
 const onStageStatisticsAlternationPeriodMax = 10;
 const onStageStatisticsAlternationPeriodDivisions =
     onStageStatisticsAlternationPeriodMax - onStageStatisticsAlternationPeriodMin;
