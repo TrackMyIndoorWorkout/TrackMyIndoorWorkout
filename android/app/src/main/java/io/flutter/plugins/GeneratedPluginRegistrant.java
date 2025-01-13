f81ac29506789def99751738b0f8e6d38601a2c0
MrCsabaToth <csaba.toth.us@gmail.com>
2025-01-12 18:49:11 -0800

    Initial implementation for receiving file by sharing intents and "Open with" #542 It works with Google Drive "Open with". But it doesn't work while trying the activity CSVs in the Download folder with the File manager (neither Share intent or Open with).

--- android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -81,6 +81,11 @@ public final class GeneratedPluginRegistrant {
       Log.e(TAG, "Error registering plugin path_provider_android, io.flutter.plugins.pathprovider.PathProviderPlugin", e);
     }
     try {
+      flutterEngine.getPlugins().add(new com.kasem.receive_sharing_intent.ReceiveSharingIntentPlugin());
+    } catch (Exception e) {
+      Log.e(TAG, "Error registering plugin receive_sharing_intent, com.kasem.receive_sharing_intent.ReceiveSharingIntentPlugin", e);
+    }
+    try {
       flutterEngine.getPlugins().add(new com.twwm.share_files_and_screenshot_widgets.ShareFilesAndScreenshotWidgetsPlugin());
     } catch (Exception e) {
       Log.e(TAG, "Error registering plugin share_files_and_screenshot_widgets, com.twwm.share_files_and_screenshot_widgets.ShareFilesAndScreenshotWidgetsPlugin", e);
--- lib/track_my_indoor_exercise_app.dart
+++ lib/track_my_indoor_exercise_app.dart
@@ -1,8 +1,16 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:isar/isar.dart';
 import 'package:pref/pref.dart';
+import 'package:receive_sharing_intent/receive_sharing_intent.dart';
 
+import 'import/csv_importer.dart';
+import 'persistence/workout_summary.dart';
+import 'preferences/leaderboard_and_rank.dart';
 import 'preferences/show_performance_overlay.dart';
 import 'ui/find_devices.dart';
 import 'utils/theme_manager.dart';
@@ -18,11 +26,69 @@ class TrackMyIndoorExerciseApp extends StatefulWidget {
 
 class TrackMyIndoorExerciseAppState extends State<TrackMyIndoorExerciseApp> {
   ThemeManager? _themeManager;
+  bool _leaderboardFeature = leaderboardFeatureDefault;
+  late StreamSubscription _intentSub;
+
+  Future<int> _importActivities(List<SharedMediaFile> sharedFiles) async {
+    var importedFileCount = 0;
+    for (final sharedFile in sharedFiles) {
+      if (!sharedFile.path.toLowerCase().endsWith(".csv")) {
+        continue;
+      }
+
+      File file = File(sharedFile.path);
+      String contents = await file.readAsString();
+      final importer = CSVImporter(null);
+      final activity = await importer.import(contents, (progress) {});
+      if (activity != null) {
+        importedFileCount++;
+        Get.snackbar("Success", "Workout imported!");
+        if (_leaderboardFeature) {
+          final deviceDescriptor = activity.deviceDescriptor();
+          final workoutSummary = activity
+              .getWorkoutSummary(deviceDescriptor.manufacturerNamePart);
+          final database = Get.find<Isar>();
+          database.writeTxnSync(() {
+            database.workoutSummarys.putSync(workoutSummary);
+          });
+        }
+      } else {
+        Get.snackbar(
+            "Failure", "Problem while importing: ${importer.message}");
+      }
+    }
+    return importedFileCount;
+  }
 
   @override
   void initState() {
     super.initState();
     _themeManager = Get.put<ThemeManager>(ThemeManager(), permanent: true);
+    _leaderboardFeature = widget.prefService.get<bool>(leaderboardFeatureTag) ?? leaderboardFeatureDefault;
+
+    // Listen to media sharing coming from outside the app while the app is in the memory.
+    _intentSub = ReceiveSharingIntent.instance.getMediaStream().listen((value) {
+      WidgetsBinding.instance.addPostFrameCallback((_) async {
+        await _importActivities(value);
+      });
+    }, onError: (err) {
+      debugPrint("getIntentDataStream error: $err");
+    });
+
+    // Get the media sharing coming from outside the app while the app is closed.
+    ReceiveSharingIntent.instance.getInitialMedia().then((value) {
+      WidgetsBinding.instance.addPostFrameCallback((_) async {
+        await _importActivities(value);
+        // Tell the library that we are done processing the intent.
+        ReceiveSharingIntent.instance.reset();
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _intentSub.cancel();
+    super.dispose();
   }
 
   @override
