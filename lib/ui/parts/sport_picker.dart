c1de907af263d455702266df4f8e76b7c9cd961d
MrCsabaToth <csaba.toth.us@gmail.com>
2025-08-31 11:46:59 -0700

    Upgrading packages along with some code (due to linting / deprecation)

--- lib/ui/parts/sport_picker.dart
+++ lib/ui/parts/sport_picker.dart
@@ -35,48 +35,45 @@ class SportPickerBottomSheetState extends State<SportPickerBottomSheet> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: widget.sportChoices
-            .asMap()
-            .entries
-            .map(
-              (e) => Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Transform.scale(
-                    scale: 2,
-                    child: Radio(
-                      value: e.key,
-                      groupValue: _sportIndex,
-                      onChanged: (value) {
+      body: RadioGroup(
+        groupValue: _sportIndex,
+        onChanged: (value) {
+          setState(() {
+            _sportIndex = value as int;
+          });
+        },
+        child: ListView(
+          children: widget.sportChoices
+              .asMap()
+              .entries
+              .map(
+                (e) => Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Transform.scale(scale: 2, child: Radio(value: e.key)),
+                    TextButton.icon(
+                      onPressed: () {
                         setState(() {
-                          _sportIndex = value as int;
+                          _sportIndex = e.key;
                         });
                       },
+                      icon: _themeManager.getBlueIcon(
+                        getSportIcon(e.value),
+                        _largerTextStyle.fontSize!,
+                      ),
+                      label: TextOneLine(
+                        e.value,
+                        style: _sportIndex == e.key ? _selectedTextStyle : _largerTextStyle,
+                        textAlign: TextAlign.left,
+                        overflow: TextOverflow.ellipsis,
+                      ),
                     ),
-                  ),
-                  TextButton.icon(
-                    onPressed: () {
-                      setState(() {
-                        _sportIndex = e.key;
-                      });
-                    },
-                    icon: _themeManager.getBlueIcon(
-                      getSportIcon(e.value),
-                      _largerTextStyle.fontSize!,
-                    ),
-                    label: TextOneLine(
-                      e.value,
-                      style: _sportIndex == e.key ? _selectedTextStyle : _largerTextStyle,
-                      textAlign: TextAlign.left,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                ],
-              ),
-            )
-            .toList(growable: false),
+                  ],
+                ),
+              )
+              .toList(growable: false),
+        ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
       floatingActionButton: _themeManager.getGreenFab(
