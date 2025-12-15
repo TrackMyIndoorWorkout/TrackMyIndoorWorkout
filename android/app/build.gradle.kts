e6ccedf938f48d766ee3d634eafc417baedaa7ff
MrCsabaToth <csaba.toth.us@gmail.com>
2025-12-14 19:49:52 -0800

    fix: correcting application ID after Android recreation #566

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.track_my_indoor_exercise"
+    namespace = "dev.csaba.track_my_indoor_exercise"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -20,7 +20,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.track_my_indoor_exercise"
+        applicationId = "dev.csaba.track_my_indoor_exercise"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
