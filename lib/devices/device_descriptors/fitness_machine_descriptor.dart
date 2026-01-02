cf00b89a37baf43c6b8dfeb6c43ee8b071bcff4c
David C <code@davidc.xyz>
2026-01-01 17:35:57 -0800

    Formatting.

--- lib/devices/device_descriptors/fitness_machine_descriptor.dart
+++ lib/devices/device_descriptors/fitness_machine_descriptor.dart
@@ -197,7 +197,11 @@ abstract class FitnessMachineDescriptor extends DeviceDescriptor {
   int processInclinationFlag(int flag) {
     if (flag % 2 == 1) {
       // SInt16, Inclination (percentage with 0.1 resolution)
-      inclinationMetric = ShortMetricDescriptor(lsb: byteCounter, msb: byteCounter + 1, divider: 10.0);
+      inclinationMetric = ShortMetricDescriptor(
+        lsb: byteCounter,
+        msb: byteCounter + 1,
+        divider: 10.0,
+      );
       byteCounter += 2;
       // SInt16, Ramp Angle (percentage with 0.1 resolution) - skipped for now
       byteCounter += 2;
